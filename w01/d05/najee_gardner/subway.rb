59f0d72eaf0dd567122bfb1647b27c4290925c3f
truotaku <najee.s.gardner@gmail.com>
2014-06-13 16:20:29 -0400

    |subway.rb| created Subway class

---
+++ w01/d05/najee_gardner/subway.rb
@@ -0,0 +1,32 @@
+require 'pry'
+require_relative 'station.rb'
+require_relative 'line.rb'
+
+class Subway
+  attr_reader(:name)
+
+  def initialize(name, train_lines)
+    @name = name
+    @train_lines = train_lines
+  end
+
+  def trip_stops(line_on, station_on, line_off, station_off)
+    if line_on != line_off
+      trip1 = get_line(line_on).distance(station_on)
+      trip2 = get_line(line_off).distance(station_off)
+      trip1 + trip2
+    else
+      get_line(line_on).distance(station_on, station_off)
+    end
+
+  end
+
+  private
+
+  attr_reader(:train_lines)
+
+  def get_line(line_name)
+    (train_lines.select { |line| line.name == line_name })[0]
+  end
+
+end
