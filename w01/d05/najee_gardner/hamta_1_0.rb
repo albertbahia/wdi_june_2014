6356e041fa4d6373bd63d397a68b98c91b9da0d4
truotaku <najee.s.gardner@gmail.com>
2014-06-13 17:10:28 -0400

    |hamta_1_0| created Subway object

--- w01/d05/najee_gardner/hamta_1_0.rb
+++ w01/d05/najee_gardner/hamta_1_0.rb
@@ -1,16 +1,42 @@
 require 'pry'
 require_relative 'station.rb'
 require_relative 'line.rb'
+require_relative 'subway.rb'
 
-stations = [
+l_stations = [
   Station.new("8th Ave", ["Bill", "Bob", "Billy"], true, 3),
   Station.new("6th Ave", ["Sam", "Sally"], true, 5),
   Station.new("Union Square", ["Tim", "Tommy", "Thomas", "Timothy"], false, 1),
   Station.new("3rd Ave", ["Gregory", "Gary", "George"], true, 10),
-  Station.new("1st Ave", ["Jill"], false, 13)
+  Station.new("1st Ave", ["Jill"], false, 13),
+  Station.new("Bedford Ave", ["Jill"], false, 13)
 ]
 
-l_train = Line.new("l", stations)
+s_stations = [
+  Station.new("Times Square", ["Bill", "Bob", "Billy"], true, 5),
+  Station.new("Herald Squre", ["Sam", "Sally"], true, 3),
+  Station.new("28th St", ["Tim", "Tommy", "Thomas", "Timothy"], false, 2),
+  Station.new("23rd St - HT", ["Gregory", "Gary", "George"], true, 30),
+  Station.new("Union Square", ["Jill"], false, 15),
+  Station.new("8th St", ["Jill"], false, 7)
+]
+
+n_stations = [
+  Station.new("Grand Central", ["Bill", "Bob", "Billy"], true, 3),
+  Station.new("33rd St", ["Sam", "Sally"], true, 5),
+  Station.new("28th St", ["Tim", "Tommy", "Thomas", "Timothy"], false, 1),
+  Station.new("23rd St", ["Gregory", "Gary", "George"], true, 10),
+  Station.new("Union Square", ["Jill"], false, 13),
+  Station.new("Astor Place", ["Jill"], false, 13)
+]
+
+lines = [
+  Line.new("l", l_stations),
+  Line.new("l", n_stations),
+  Line.new("l", _stations)
+]
+
+mta = Subway.new("mta", lines)
 
 def display_heading(title)
   puts "=" * 30
