72a32b59efe7ee13f9cd1962fec6c5b5e94873d9
hoanewton <caohoa86hp@gmail.com>
2014-06-14 13:37:28 -0400

    finish class trainer

--- w01/d05/hoa_newton/trainer.rb
+++ w01/d05/hoa_newton/trainer.rb
@@ -8,10 +8,25 @@ class Trainer
 		@pokemons = pokemons
 	end
 
-	def list_pokemons(pokemons) # list all pokemons from 1 trainer
+	def list_pokemons(pokemons_arr) # list all pokemons from 1 trainer
 			puts "Trainer #{@name} has pokemons: "
-		pokemons.each do |pokemon|
+		pokemons_arr.each do |pokemon|
 			puts pokemon.name
+		end
+	end
+
+	def trainer_info
+		puts "Trainer's name: #{@name}"
+		puts "Age: #{@age}"
+		puts "Hometown: #{@hometown}"
+	end
+
+	def add_poke(poke, pokes)
+		if pokes.length >= 6
+			puts "You can not own more than 6 pokemons!"
+		else
+			pokes.push(poke)
+			puts "You just added #{poke.name}"
 		end
 	end
 
