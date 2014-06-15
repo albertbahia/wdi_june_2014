c441188f6b94ca1db5fe1a848f7e366b6134bb24
ericfrommelt <ericfrommelt@gmail.com>
2014-06-15 19:19:27 -0400

    initial nurse class

---
+++ w01/d05/eric_frommelt/nurse.rb
@@ -0,0 +1,17 @@
+class Nurse
+
+  def initialize(name, age, hometown)
+    @name = name
+    @age = age
+    @hometown = hometown
+  end
+
+  def list_nurse_info
+    nurse_info = "Name: #{name} Age: #{age} Hometown: #{hometown}"
+  end
+
+  def heal_pokemons(trainer.name)
+    
+  end
+
+end
