0500f1cb9183b707fa1e28d491c554408f055ce9
ericfrommelt <ericfrommelt@gmail.com>
2014-06-15 18:57:27 -0400

    initial trainer class

---
+++ w01/d05/eric_frommelt/trainer.rb
@@ -0,0 +1,22 @@
+class Trainer
+
+  def initialize(name, age, hometown, pokemons)
+    @name = name
+    @age = age
+    @hometown = hometown
+    @pokemons = pokemons
+  end
+
+  def list_trainer_info
+    trainer_info = "Name: #{name} Age: #{age} Hometown: #{hometown}"
+  end
+
+  def add_pokemon
+
+  end
+
+  def list_pokemons
+    
+  end
+
+end
