c07f1bce14fb05eb147cb10a4aa2035c92332d16
alevsxm <alexmlevine@gmail.com>
2014-06-15 14:10:08 -0400

    added trainer class

---
+++ w01/d05/alexander_levine/trainer.rb
@@ -0,0 +1,41 @@
+class Trainer
+
+  attr_reader :name, :age, :hometown, :pokemons
+
+  def initialize(name, age, hometown, pokemons)
+    @name = name
+    @age = age
+    @hometown = hometown
+    @pokemons = pokemons
+  end
+
+  def list_info
+    info = ["name: #{name}", "age: #{age}", "hometown: #{hometown}",
+      "pokemons: #{pokemons}"]
+  end
+
+  def add_pokemon(pokemon_name)
+    if pokemons.length < 6
+      @pokemons.push(pokemon_name)
+    else
+      return "Can only hold maximum of 6 pokemon!"
+    end
+  end
+
+end
+
+
+
+
+# #### Trainer class
+# A trainer should have a:
+# - name
+# - age
+# - hometown
+# - pokemons
+#
+# A trainer should be able to:
+# - list their pokemons
+# - list their info
+# - add a pokemon
+#   - can not have more than 6
