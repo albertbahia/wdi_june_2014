1c9ed40a51b67b51b479080a6061d7fba72253da
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-06-16 19:55:27 -0400

    Completed Assignment

--- w02/d01/gadi_gottlieb/ASSIGNMENT_FILES/GoT/lib/beast.rb
+++ w02/d01/gadi_gottlieb/ASSIGNMENT_FILES/GoT/lib/beast.rb
@@ -1,2 +1,47 @@
 class Beast
+
+  def initialize(name, species, strength)
+    @name = name
+    @species = species
+    @strength = strength
+    @hp = 100
+  end
+
+  def name
+    return @name
+  end
+
+  def species
+    return @species
+  end
+
+  def strength
+    return @strength
+  end
+
+  def hp
+    @hp
+  end
+
+  def take_damage(damage_taken)
+    if damage_taken < 0
+    elsif @hp - damage_taken < 0
+      @hp = 0
+    else @hp - damage_taken > 0
+      @hp -= damage_taken
+    end
+end
+
+  def attack(victim)
+    victim.take_damage(@strength)
+  end
+
+  def alive?
+    if @hp > 0
+      true
+    else
+      false
+    end
+  end
+
 end
--- w02/d01/gadi_gottlieb/ASSIGNMENT_FILES/GoT/lib/human.rb
+++ w02/d01/gadi_gottlieb/ASSIGNMENT_FILES/GoT/lib/human.rb
@@ -1,2 +1,50 @@
 class Human
+
+  def initialize(name, house, strength)
+    @name = name
+    @house = house
+    @strength = strength
+    @hp = 200
+  end
+
+  def name
+    return @name
+  end
+
+  def house
+    return @house
+  end
+
+  def strength
+    return @strength
+  end
+
+  def hp
+    return @hp
+  end
+
+  def introduce
+    "Hi my name is #{@name} and I have a house in #{@house}."
+  end
+
+  def take_damage(damage_taken)
+    if damage_taken < 0
+    elsif @hp - damage_taken < 0
+      @hp = 0
+    else @hp - damage_taken > 0
+      @hp -= damage_taken
+    end
+  end
+
+  def attack(victim)
+    victim.take_damage(@strength)
+  end
+
+  def alive?
+    if hp > 0
+      true
+    else
+      false
+    end
+  end
 end
