922cdb62d024b76af15d4b805c07d3d8cd561d08
truotaku <najee.s.gardner@gmail.com>
2014-06-16 19:06:48 -0400

    beast class returns no errors

--- w02/d01/najee_gardner/GoT/lib/beast.rb
+++ w02/d01/najee_gardner/GoT/lib/beast.rb
@@ -1,2 +1,30 @@
 class Beast
+  attr_reader :name, :species, :strength, :hp
+
+  def initialize(name, species, strength)
+    @name = name
+    @species = species
+    @strength = strength
+    @hp = 100
+  end
+
+  def take_damage(amount)
+    if amount > 100
+      @hp = 0
+    elsif amount > 0
+      @hp = hp - amount
+    end
+  end
+
+  def attack(enemy)
+    enemy.take_damage(strength)
+  end
+
+  def alive?
+    if hp > 0
+      true
+    else
+      false
+    end
+  end
 end
