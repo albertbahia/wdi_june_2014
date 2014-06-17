f3a42a21ea77f5fdf3cbed1819f0184265ef933c
truotaku <najee.s.gardner@gmail.com>
2014-06-16 20:14:39 -0400

    human class passes tests

--- w02/d01/najee_gardner/GoT/lib/human.rb
+++ w02/d01/najee_gardner/GoT/lib/human.rb
@@ -1,2 +1,30 @@
 class Human
+  attr_reader :name, :house, :strength, :hp
+
+  def initialize(name, house, strength)
+    @name = name
+    @house = house
+    @strength = strength
+    @hp = 200
+  end
+
+  def introduce
+    "I'm #{name.capitalize} of house #{house.capitalize}."
+  end
+
+  def take_damage(damage)
+    if damage > 200
+      @hp = 0
+    elsif damage > 0
+      @hp -= damage
+    end
+  end
+
+  def attack(enemy)
+    enemy.take_damage(strength)
+  end
+
+  def alive?
+    hp > 0 ? true : false
+  end
 end
