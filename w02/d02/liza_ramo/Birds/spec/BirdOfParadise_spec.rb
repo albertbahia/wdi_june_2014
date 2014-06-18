729121d9cf60eb9d1b613b67f58be54162c808e9
LizaLemons <liza.h.ramo@gmail.com>
2014-06-17 21:53:24 -0400

    Added tests in BirdOfParadise.rb

---
+++ w02/d02/liza_ramo/Birds/spec/BirdOfParadise_spec.rb
@@ -0,0 +1,42 @@
+require_relative '../lib/BirdOfParadise.rb'
+
+describe BirdOfParadise do
+  let(:snoopy) { BirdOfParadise.new('Snoopy', 13, 'tropics', 7, 'red')}
+
+  it 'has a name' do
+    expect(snoopy.name).to eq('Snoopy')
+  end
+
+  it 'has an age' do
+    expect(snoopy.age).to eq(13)
+  end
+
+  it 'has a location' do
+    expect(snoopy.location).to eq('tropics')
+  end
+
+  it 'has a speed' do
+    expect(snoopy.speed).to eq(7)
+  end
+
+   it 'has a color' do
+     expect(snoopy.color).to eq('red')
+   end
+
+   describe '#get_food' do
+     it 'got food' do
+       expect(snoopy.get_food).to eq(true)
+     end
+   end
+
+    describe '#fly' do
+      it 'flew' do
+        expect(snoopy.fly).to eq('Im flying!')
+    end
+
+    describe '#bathes' do
+      it 'bathed' do
+      expect(snoopy.bathes).to eq('Im very clean!')
+    end
+
+end
--- w02/d02/liza_ramo/Birds/spec/Bird_spec.rb
+++ w02/d02/liza_ramo/Birds/spec/Bird_spec.rb
@@ -1,30 +1,30 @@
 
-require_relative '../lib/Bird.rb'
-
-describe Bird do
-  let(:snoopy) { Bird.new("Snoopy", 13, "tropics", 7)}
-
-  it 'has a name' do
-    expect(snoopy.name).to eq('Snoopy')
-  end
-
-  it 'has an age' do
-    expect(snoopy.age).to eq(13)
-  end
-
-  it 'has a location' do
-    expect(snoopy.location).to eq('tropics')
-  end
-
-  it 'has a speed' do
-    expect(snoopy.speed).to eq(7)
-  end
-
-
-  describe '#get_food' do
-    it 'got food' do
-      expect(snoopy.get_food).to eq(true)
-    end
-  end
-
-end
+# require_relative '../lib/Bird.rb'
+#
+# describe Bird do
+#   let(:snoopy) { Bird.new("Snoopy", 13, "tropics", 7)}
+#
+#   it 'has a name' do
+#     expect(snoopy.name).to eq('Snoopy')
+#   end
+#
+#   it 'has an age' do
+#     expect(snoopy.age).to eq(13)
+#   end
+#
+#   it 'has a location' do
+#     expect(snoopy.location).to eq('tropics')
+#   end
+#
+#   it 'has a speed' do
+#     expect(snoopy.speed).to eq(7)
+#   end
+#
+#
+#   describe '#get_food' do
+#     it 'got food' do
+#       expect(snoopy.get_food).to eq(true)
+#     end
+#   end
+#
+# end
