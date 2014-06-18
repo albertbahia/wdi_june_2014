345e8fe50275468988b11d70f8f5c1cb7e225704
alevsxm <alexmlevine@gmail.com>
2014-06-17 22:09:24 -0400

    added elf spec and class

---
+++ w02/d02/alexander_levine/LoTR/lib/elf.rb
@@ -0,0 +1,18 @@
+class Elf < MiddleEarthDweller
+
+  attr_reader :flowers_sprouted
+
+  def initalize(name, hometown, favorite_town)
+    super(name, hometown, favorite_town)
+    @flowers_sprouted = 0
+  end
+
+  def beautify
+    @flowers_sprouted += 1
+  end
+
+  def pass_over_sea
+    @hometown = "The West"
+  end
+
+end
