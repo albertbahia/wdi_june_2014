5a60dcae32786098d0dc13c3795806127080d2ac
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-06-18 10:46:09 -0700

    start up files for yesterdays hw

---
+++ w02/d02/khadijah_celestine/hw/spec/ga_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'spec_helper'
+require_relative '../lib/ga'
+
+describe '###ga initialize' do
+  it 'has a location' do
+  end
+
+  it 'has a start month' do
+  end
+
+  it 'has a start year' do
+  end
+end
