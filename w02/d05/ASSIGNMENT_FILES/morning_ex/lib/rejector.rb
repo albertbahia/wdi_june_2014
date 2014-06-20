e21f29f7939dd430bbc04f5bab13e3f838ea18b8
DrRobotmck <mckenneth.scott@icloud.com>
2014-06-20 16:41:58 -0400

    hw for the weekend

--- w02/d05/ASSIGNMENT_FILES/morning_ex/lib/rejector.rb
+++ w02/d05/ASSIGNMENT_FILES/morning_ex/lib/rejector.rb
@@ -8,3 +8,8 @@ def rejector(array_of_values)
   array_of_values.reject { |string| string.to_i.odd? }
   .reduce(0) { |sum, n| sum + n.to_i }
 end
+
+# def rejector(array)
+#   array.map {|num| num.to_i.even? ? num.to_i : 0 }
+#   .reduce(:+)
+# end
