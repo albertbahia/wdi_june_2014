a398f132b2754965b0d890b5b24f6a9341fd77d4
truotaku <najee.s.gardner@gmail.com>
2014-06-21 19:47:12 -0400

    passed written tests (skipped some tests)

--- w02/d05/najee_gardner/weekend_assignment/lib/apartment.rb
+++ w02/d05/najee_gardner/weekend_assignment/lib/apartment.rb
@@ -1,3 +1,14 @@
 class Apartment
+  attr_reader :name, :floor, :price, :sqft
+  attr_reader :bedrooms, :bathrooms, :tenants
 
+  def initialize(name, floor, price, sqft, bedrooms, bathrooms)
+    @name = name
+    @floor = floor
+    @price = price
+    @sqft = sqft
+    @bedrooms = bedrooms
+    @bathrooms = bathrooms
+    @tenants = []
+  end
 end
