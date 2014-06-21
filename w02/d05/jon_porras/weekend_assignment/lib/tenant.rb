bf763da82d7744c19951826be17735300d38aba1
jonathanporras <jonathanrporras@gmail.com>
2014-06-21 11:43:35 -0400

    Completed Assignment

---
+++ w02/d05/jon_porras/weekend_assignment/lib/tenant.rb
@@ -0,0 +1,13 @@
+class Tenant
+  attr_reader(:name, :age, :gender, :apartment)
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+    @apartment = nil
+  end
+
+  def list_info
+    return "Name: #{name}, Age: #{age}, Gender #{gender}."
+  end
+end
