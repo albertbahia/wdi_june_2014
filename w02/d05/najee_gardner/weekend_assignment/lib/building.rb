f601f798df358a5b27fe7f42ee702e32d9ec114c
truotaku <najee.s.gardner@gmail.com>
2014-06-21 19:50:59 -0400

    made initial tests pass

--- w02/d05/najee_gardner/weekend_assignment/lib/building.rb
+++ w02/d05/najee_gardner/weekend_assignment/lib/building.rb
@@ -1,3 +1,10 @@
 class Building
+  attr_reader :name, :address, :floors, :apartments
 
+  def initialize(name, address, floors)
+    @name = name
+    @address = address
+    @floors = floors
+    @apartments = []
+  end
 end
