34980feb2d162a25c769898a49b1d1cff7cdcd29
truotaku <najee.s.gardner@gmail.com>
2014-06-21 01:46:47 -0400

    James and Najee tic tac toe assignment

---
+++ w02/d05/tic-tac-toe/james-najee/lib/tic_tac_toe.rb
@@ -0,0 +1,52 @@
+class TicTacToe
+
+    attr_reader(:board, :win_lines, :player_turn)
+
+    def initialize
+      @board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      @win_lines = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
+      @player_turn = :player1
+    end
+
+    def fill_spot(spot)
+      if spot.is_a?(Fixnum)
+        if spot > 8 || spot < 0
+          nil
+        else
+          board[spot] = player_turn == :player1 ? "X" : "O"
+          @player_turn = player_turn == :player1 ? :player2 : :player1
+        end
+      else
+        nil
+      end
+    end
+
+    def winner?
+      win_lines.each do |arr_index|
+        line = []
+        arr_index.each do |index|
+          line << board[index]
+        end
+
+        winner = line.reject {|move| (0..8).include?(move)}.join
+
+        if winner == "XXX" || winner == "OOO"
+          return true
+        end
+      end
+
+      false
+    end
+
+
+
+end
+
+# describe '#fill_spot' do
+#   it 'should fill a spot on the board'
+#     ttt_game.fill_spot("x", 2)
+#     expect(ttt_game.board).to eq([nil, nil, "x", nil, nil, nil, nil, nil, nil])
+#   end
+#
+#   it 'should return nil if invalid arguments argument' do
+#     expect(ttt_game.fill_spot(1, 9)).to eq(nil)
---
+++ w02/d05/tic-tac-toe/james-najee/spec/tic_tac_toe_spec.rb
@@ -0,0 +1,71 @@
+require_relative '../lib/tic_tac_toe.rb'
+
+describe TicTacToe do
+  let(:ttt_game) { TicTacToe.new }
+
+  it 'should have empty a board' do
+    empty_board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    expect(ttt_game.board).to eq(empty_board)
+  end
+
+  it 'should have win_lines' do
+    test_win_lines = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
+    expect(ttt_game.win_lines).to eq(test_win_lines)
+  end
+
+  it 'should have a player_turn' do
+    expect(ttt_game.player_turn).to eq(:player1)
+  end
+
+  describe '#fill_spot' do
+    it 'should fill a spot on the board' do
+      ttt_game.fill_spot(2)
+      expect(ttt_game.board).to eq([0, 1, "X", 3, 4, 5, 6, 7, 8])
+    end
+
+    it 'should return nil if wrong argument' do
+      expect(ttt_game.fill_spot(9)).to eq(nil)
+      expect(ttt_game.fill_spot("hello")).to eq(nil)
+    end
+
+    it 'should change player1 to player2' do
+      ttt_game.fill_spot(2)
+      expect(ttt_game.player_turn).to eq(:player2)
+    end
+
+    it 'should change player2 to player1' do
+      ttt_game.fill_spot(5)
+      ttt_game.fill_spot(2)
+      expect(ttt_game.player_turn).to eq(:player1)
+    end
+
+    it 'should place an "x" on player1 turn' do
+      ttt_game.fill_spot(5)
+      expect(ttt_game.board).to eq([0, 1, 2, 3, 4, "X", 6, 7, 8])
+
+    end
+
+    it 'should place an "o" on player2 turn' do
+      ttt_game.fill_spot(5)
+      ttt_game.fill_spot(2)
+      expect(ttt_game.board).to eq([0, 1, "O", 3, 4, "X", 6, 7, 8])
+    end
+
+    it 'should not change player_turn if wrong argument is passed' do
+      ttt_game.fill_spot(9)
+      expect(ttt_game.player_turn).to eq(:player1)
+    end
+  end
+
+  describe '#winner?' do
+    it 'should return true if player gets tic tac toe' do
+      ttt_game.fill_spot(2)
+      ttt_game.fill_spot(1)
+      ttt_game.fill_spot(5)
+      ttt_game.fill_spot(7)
+      ttt_game.fill_spot(8)
+      expect(ttt_game.winner?).to eq(true)
+    end
+  end
+
+end
