51c1724dba1535595b772aecac1a4f8b45bc66b3
fgc29 <fgc0077@gmail.com>
2014-06-22 15:01:57 -0400

    added building class

---
+++ w02/d05/francis_castillo/weekend_assignment/lib/building.rb
@@ -0,0 +1,33 @@
+# require_relative '../lib/apartment'
+# require_relative '../lib/tenant'
+class Building
+
+  def initialize (name, address, floors)
+    @name = name
+    @address = address
+    @floors = floors
+    # @apartments = Apartment.new
+    # # @tenants = [Tenant.new]
+  end
+
+  def name
+    @name
+  end
+
+  def address
+    @address
+  end
+
+  def floors
+    @floors
+  end
+
+  # def apartment
+  #   @apartment
+  # end
+
+  # def tenant
+  #   @tenant
+  # end
+
+end
