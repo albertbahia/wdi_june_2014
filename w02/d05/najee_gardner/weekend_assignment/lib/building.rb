3cf61fb180e628d023de839c96a9e7a7b06042b0
truotaku <najee.s.gardner@gmail.com>
2014-06-22 10:19:01 -0400

    passes all tests

--- w02/d05/najee_gardner/weekend_assignment/lib/building.rb
+++ w02/d05/najee_gardner/weekend_assignment/lib/building.rb
@@ -7,4 +7,20 @@ class Building
     @floors = floors
     @apartments = []
   end
+
+  def list_apartments
+    apt_info = ""
+    apartments.each do |apt|
+      apt_info << apt.info
+    end
+    apt_info
+  end
+
+  def rent_apartment(tenant, apt_name)
+    tenant_apt = apartments.select {|apt| apt.name == apt_name}.first
+    if tenant_apt.tenants.length < tenant_apt.bedrooms
+      tenant.apartment = tenant_apt.name
+      tenant_apt.tenants.push(tenant)
+    end
+  end
 end
