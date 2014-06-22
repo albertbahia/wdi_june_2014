6a400707f78db3290159be1389fd981fe721b76e
MALRMALR <michaelalross@gmail.com>
2014-06-22 16:07:46 -0400

    building class and spec

---
+++ w02/d05/michael_ross/weekend_assignment/lib/building.rb
@@ -0,0 +1,33 @@
+require_relative 'apartment.rb'
+require_relative 'tenant.rb'
+
+
+class Building
+
+  attr_reader(:name, :address, :floors, :apartments)
+
+  def initialize(name, address, floors)
+    @name = name
+    @address = address
+    @floors = floors
+    @apartments = []
+  end
+
+  def list_apartments
+    info_string = ""
+    apartments.each { |apartment|
+    return apartment.info
+    }
+  end
+
+  def rent_apartment(tenant, apartment)
+    if apartments.include?(apartment)
+      "Apartment has been rented."
+    else
+      apartment.add_tenant(tenant)
+      @apartments.push(apartment)
+    end
+
+  end
+
+end
