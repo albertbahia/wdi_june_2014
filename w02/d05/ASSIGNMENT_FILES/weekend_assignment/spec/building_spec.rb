21c8e6c75b270097dea3b73cd8856b78384b821a
McKenneth Scott III <mckenneth.scott@icloud.com>
2014-06-23 13:59:02 -0400

    Update building_spec.rb

--- w02/d05/ASSIGNMENT_FILES/weekend_assignment/spec/building_spec.rb
+++ w02/d05/ASSIGNMENT_FILES/weekend_assignment/spec/building_spec.rb
@@ -1,8 +1,6 @@
 require_relative '../lib/building'
 require_relative '../lib/apartment'
 require_relative '../lib/tenant'
-require_relative'../main.rb'
-
 
 describe Building do
   subject(:building) { Building.new("HAMco Towers", "41 USW NY,NY", 5)}
