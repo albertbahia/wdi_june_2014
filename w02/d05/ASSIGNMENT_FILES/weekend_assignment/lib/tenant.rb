90e89e1bdd32f40ce5197a7b1b2c86d8bf0e707a
ppakdev <ppak73@gmail.com>
2014-06-22 21:22:19 -0400

    forgot to do after each class. passed tests

---
+++ w02/d05/ASSIGNMENT_FILES/weekend_assignment/lib/tenant.rb
@@ -0,0 +1,16 @@
+class Tenant
+
+  attr_reader(:name, :age, :gender, :apartment)
+
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+    @apartment = nil
+  end
+
+  def list_info
+    info_string = "Name: #{name}, Age: #{age}, Gender: #{gender}"
+  end
+
+end
