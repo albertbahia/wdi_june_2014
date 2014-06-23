5b8d58d938680426408825e78c4b6d34c9b6b091
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-06-22 22:15:52 -0700

    apartment and building are passing some tests

--- w02/d05/khadijah_celestine/weekend_assignment/lib/apartment.rb
+++ w02/d05/khadijah_celestine/weekend_assignment/lib/apartment.rb
@@ -1,3 +1,12 @@
 class Apartment
+	attr_reader(:floor, :name, :price, :sqft, :bedrooms, :bathrooms)
+	def initialize(floor, name, price, sqft, bedrooms, bathrooms)
+		@floor = floor
+		@name = name
+		@price = price
+		@sqft = sqft
+		@bedrooms = bedrooms
+		@bathrooms = bathrooms
+	end
 
 end
--- w02/d05/khadijah_celestine/weekend_assignment/lib/building.rb
+++ w02/d05/khadijah_celestine/weekend_assignment/lib/building.rb
@@ -1,3 +1,10 @@
 class Building
 
+	attr_reader(:name, :address, :floors)
+	def initialize(name, address, floors)
+		@name = name
+		@address = address
+		@floors = floors
+	end
+
 end
