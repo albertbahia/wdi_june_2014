1006869d85bfa485e6f450561dac5eaf0f71c107
albertbahia <albert.bahia1@gmail.com>
2014-06-22 22:16:34 -0400

    Completed Apartment spec and class.

---
+++ w02/d05/albert_bahia/weekend_assignment/lib/apartment.rb
@@ -0,0 +1,33 @@
+require 'pry'
+# require_relative './tenant.rb'
+
+
+class Apartment
+
+  attr_reader(:name, :floor, :price, :sqft, :bedrooms, :bathrooms, :tenants)
+
+  def initialize(name, floor, price, sqft, bedrooms,bathrooms)
+    @name = name
+    @floor = floor
+    @price = price
+    @sqft = sqft
+    @bedrooms = bedrooms
+    @bathrooms = bathrooms
+    @tenants = []
+  end
+
+  def info(apartment)
+    return apartment
+  end
+
+  def list_tenants(tenant_1, tenant_2, tenant_3)
+    @tenants = [tenant_1, tenant_2, tenant_3]
+    return @tenants
+  end
+
+  def add_tenant(new_tenant)
+    new_tenant = Tenant.new(@name, @age, @gender)
+    @tenants.push(new_tenant)
+  end
+
+end
