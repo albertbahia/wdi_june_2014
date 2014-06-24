969b48c346d763b750ba680bafcce586391b3e28
MeetElizabeth <ecaberne@gmail.com>
2014-06-24 19:53:30 -0400

    finished homework assignment

--- w03/d02/elizabeth_abernethy/hw_w03_d02/db/trainer_seed.rb
+++ w03/d02/elizabeth_abernethy/hw_w03_d02/db/trainer_seed.rb
@@ -1,16 +1,13 @@
-require_relative '../lib/trainer.rb'
-require 'pry'
-
-trainer_names = %w(Ash Misty Brock Gary Lorelei Bruno Agatha Lance)
-trainer_ages = [10,13,15,25,76,32,89,345,21,9001]
-trainer_hometown = ['Pallet Town','Saffron City', 'Cerulean City','Celadon City', 'Cinnabar Island', 'Fuschia City', 'Goldenrod City']
-
-10.times do |new_trainer|
-  new_trainer = Trainer.create ({
-    name: trainer_names.sample,
-    age: trainer_ages.sample,
-    hometown: trainer_hometown.sample,
-    })
-end
-
-binding.pry
+# require_relative '../lib/trainer.rb'
+#
+# trainer_names = %w(Ash Misty Brock Gary Lorelei Bruno Agatha Lance)
+# trainer_ages = [10,13,15,25,76,32,89,345,21,9001]
+# trainer_hometown = ['Pallet Town','Saffron City', 'Cerulean City','Celadon City', 'Cinnabar Island', 'Fuschia City', 'Goldenrod City']
+#
+# 10.times do |new_trainer|
+#   new_trainer = Trainer.create ({
+#     name: trainer_names.sample,
+#     age: trainer_ages.sample,
+#     hometown: trainer_hometown.sample,
+#     })
+# end
--- w03/d02/elizabeth_abernethy/hw_w03_d02/lib/trainer.rb
+++ w03/d02/elizabeth_abernethy/hw_w03_d02/lib/trainer.rb
@@ -1,9 +1,22 @@
 require 'active_record'
 
-class Trainer < ActiveRecord::Base
-end
-
 ActiveRecord::Base.establish_connection({
   database: 'pokemon_db',
   adapter: 'postgresql'
   })
+
+class Trainer < ActiveRecord::Base
+  has_and_belongs_to_many :pokemons
+
+  def whos_that_trainer
+    who_is_it = ("#{id} | #{name} | #{age} | #{hometown}")
+  end
+
+  def choose_pokemon(pokemon_array)
+    # self.pokemons << Pokemon.find_by(name: pokemon)
+    pokemon_array.each do |pokemon|
+      self.pokemons << pokemon
+    end
+  end
+
+end
