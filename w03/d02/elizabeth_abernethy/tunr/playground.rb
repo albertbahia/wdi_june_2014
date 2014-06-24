79af6434ff377fa2d3a8e9486c7af218c7f5f44a
MeetElizabeth <ecaberne@gmail.com>
2014-06-24 14:11:34 -0400

    added in-class exercise on data persistence -- making a playlist for HAMCo

--- w03/d02/elizabeth_abernethy/tunr/playground.rb
+++ w03/d02/elizabeth_abernethy/tunr/playground.rb
@@ -1,11 +1,11 @@
 require 'active_record'
 require 'pry'
-require_relative 'lib/song.rb'
-require_relative 'lib/playlist.rb'
+require_relative 'lib/song'
+require_relative 'lib/playlist'
 
 ActiveRecord::Base.establish_connection({
-  database: 'tunr_db'
+  database: 'tunr_db',
   adapter: 'postgresql'
-  })
+})
 
 binding.pry
