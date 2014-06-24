994d0cdd2b6b4fc269e3aeb836cd12e3e2a5c7fe
truotaku <najee.s.gardner@gmail.com>
2014-06-24 09:38:13 -0400

    used method that creates methods to complete calculator class

--- w03/d01/najee_gardner/calculator/lib/calculator.rb
+++ w03/d01/najee_gardner/calculator/lib/calculator.rb
@@ -1,29 +1,58 @@
 class Calculator
 
-  # def initialize
-  #   @current_number = nil
-  #   @operation = nil
-  # end
+  def initialize
+    @current_number = nil
+    @operation = nil
+  end
 
-  def five
-    case @operation
-    when nil
-      @current_number = 5
-      self
-    when :minus
-      @current_number - 5
-    when :plus
-      @current_number + 5
+  ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"].each do |number|
+	  define_method("#{number}") do
+			num = 0
+      case number
+      when "one" then num = 1
+      when "two" then num = 2
+      when "three" then num = 3
+      when "four" then num = 4
+      when "five" then num = 5
+      when "six" then num = 6
+      when "seven" then num = 7
+      when "eight" then num = 8
+      when "nine" then num = 9
+      when "ten" then num = 10
+      end
+
+      case @operation
+      when nil
+        @current_number = num
+        self
+      when :minus
+        @current_number - num
+      when :plus
+        @current_number + num
+      when :multiply
+        @current_number * num
+      when :divide
+        @current_number / num
+      end
+  	end
+  end
+
+  ["plus", "minus", "multiply", "divide"].each do |operation|
+    define_method("#{operation}") do
+      case operation
+      when "plus"
+        @operation = :plus
+        self
+      when "minus"
+        @operation = :minus
+        self
+      when "multiply"
+        @operation = :multiply
+        self
+      when "divide"
+        @operation = :divide
+        self
+      end
     end
-  end
-
-  def plus
-    @operation = :plus
-    self
-  end
-
-  def minus
-    @operation = :minus
-    self
   end
 end
--- w03/d01/najee_gardner/calculator/spec/calculator_spec.rb
+++ w03/d01/najee_gardner/calculator/spec/calculator_spec.rb
@@ -10,7 +10,8 @@ describe Calculator do
     end
   end
 
-  describe '#five' do
+
+  describe "#five" do
     it "should return an instance of class calculator" do
       expect(my_calc.five.class).to eq(Calculator)
     end
@@ -22,7 +23,36 @@ describe Calculator do
       result = my_calc.five.minus.five
       expect(result).to eq(0)
     end
+    it "should return a numerical result if multiply has already been called" do
+      result = my_calc.five.multiply.five
+      expect(result).to eq(25)
+    end
+    it "should return a numerical result if multiply has already been called" do
+      result = my_calc.five.divide.five
+      expect(result).to eq(1)
+    end
+  end
 
+  describe "#two" do
+    it "should return an instance of class calculator" do
+      expect(my_calc.two.class).to eq(Calculator)
+    end
+    it "should return a numerical result if plus has already been called" do
+      result = my_calc.one.plus.two
+      expect(result).to eq(3)
+    end
+    it "should return a numerical result if minus has already been called" do
+      result = my_calc.seven.minus.two
+      expect(result).to eq(5)
+    end
+    it "should return a numerical result if multiply has already been called" do
+      result = my_calc.eight.multiply.two
+      expect(result).to eq(16)
+    end
+    it "should return a numerical result if multiply has already been called" do
+      result = my_calc.six.divide.two
+      expect(result).to eq(3)
+    end
   end
 
   describe '#plus' do
@@ -33,6 +63,16 @@ describe Calculator do
   describe '#minus' do
     it "should return an instance of class calculator" do
       expect(my_calc.five.minus.class).to eq(Calculator)
+    end
+  end
+  describe '#multiply' do
+    it "should return an instance of class calculator" do
+      expect(my_calc.five.multiply.class).to eq(Calculator)
+    end
+  end
+  describe '#divide' do
+    it "should return an instance of class calculator" do
+      expect(my_calc.five.divide.class).to eq(Calculator)
     end
   end
 
