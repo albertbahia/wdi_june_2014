f3d8bc595381fbb4c0950d46b9bd4e31489cf033
albertbahia <albert.bahia1@gmail.com>
2014-06-24 00:11:26 -0400

    Completed Assignment

---
+++ w03/d01/albert_bahia/pokemon/pokemon_schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS pokemons;
+
+
+CREATE TABLE pokemons(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  img VARCHAR(512),
+  species VARCHAR(255),
+  hp VARCHAR(255),
+  attack VARCHAR(255),
+  defense VARCHAR(255),
+  speed VARCHAR(255),
+  moves VARCHAR(512),
+  classification VARCHAR(255),
+  height VARCHAR(255),
+  happiness VARCHAR(255)
+);
