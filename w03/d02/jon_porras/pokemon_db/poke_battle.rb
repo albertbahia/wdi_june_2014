84d177c6e832a20b29de7c9cb6a7f0316689e8a5
jonathanporras <jonathanrporras@gmail.com>
2014-06-24 21:12:35 -0400

    pokemon active record hw_w03_d02, still working on bonus

---
+++ w03/d02/jon_porras/pokemon_db/poke_battle.rb
@@ -0,0 +1,23 @@
+require 'active_record'
+require_relative 'pokemon_seed.rb'
+require_relative 'pokemon.rb'
+require_relative 'trainer.rb'
+
+ActiveRecord::Base.establish_connection({
+    database: 'pokemon_db',
+    adapter: 'postgresql'
+})
+
+num = rand(6)+1
+trainer1 = Trainer.find(num)
+trainer1.pokemons = []
+
+num = rand(6)+1
+trainer2 = Trainer.find(num)
+trainer2.pokemons = []
+
+trainer1.choose_pokemon(Pokemon.get_pokemon)
+trainer2.choose_pokemon(Pokemon.get_pokemon)
+
+trainer1.pokemons.map {|pokemon| pokemon.total_attack}
+trainer2.pokemons.map {|pokemon| pokemon.total_attack}
