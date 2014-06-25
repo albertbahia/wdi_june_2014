62cc7e6bf5dafee8dc372083f9b55d42423c30f0
ericfrommelt <ericfrommelt@gmail.com>
2014-06-24 20:39:37 -0400

    Completed assignment

--- w03/d01/eric_frommelt/pokemon/pokemon_seed_script.rb
+++ w03/d01/eric_frommelt/pokemon/pokemon_seed_script.rb
@@ -1,23 +1,23 @@
+require_relative 'pokemon.rb'
 require_relative 'pokemon_seed.rb'
 require 'pry'
 
-def poke_parse
-  my_pokemon = get_pokemon()
-  my_pokemon.each do |arr|
-    puts arr[:id]
-    puts arr[:name]
-    puts arr[:img]
-    puts arr[:stats][:hp]
-    puts arr[:stats][:attack]
-    puts arr[:stats][:defense]
-    puts arr[:stats][:speed]
-    puts arr[:moves][:level][1][:name]
-    puts arr[:classification]
-    puts arr[:species]
-    puts arr[:height]
-    puts arr[:happiness]
-  end
-end
+my_pokemon = get_pokemon()
 
+my_pokemon.each do |pokemon_hash|
+  Pokemon.create({
+    name: pokemon_hash[:name],
+    img_url: pokemon_hash[:img],
+    hp: pokemon_hash[:stats][:hp].to_i,
+    attack: pokemon_hash[:stats][:attack].to_i,
+    defense: pokemon_hash[:stats][:defense].to_i,
+    speed: pokemon_hash[:stats][:speed].to_i,
+    moves: pokemon_hash[:moves][:level].map { |move| move[:name].capitalize }.join(', '),
+    classification: pokemon_hash[:misc][:classification],
+    species: pokemon_hash[:type].join(', '),
+    height: pokemon_hash[:height],
+    happiness: pokemon_hash[:misc][:happiness].to_i
+  })
+end
 
 binding.pry
