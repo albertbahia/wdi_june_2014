19f28dc4b651974b46bf0a6a8eab552e219e4936
charlyebrown <tran1226@yahoo.com>
2014-06-25 00:04:18 -0400

    finished homework

--- w03/d02/charlye_tran/pokemon_trainer/lib/trainer.rb
+++ w03/d02/charlye_tran/pokemon_trainer/lib/trainer.rb
@@ -1,8 +1,17 @@
 require 'active_record'
-#require 'pry'
+require 'pokemon.rb'
+require 'pry'
 
 class Trainer < ActiveRecord::Base
   has_and_belongs_to_many :pokemons
+
+  def whos_that_trainer
+    return self
+  end
+
+  def choose_pokemon(pokemon)
+    self.pokemons << pokemon
+  end
 end
 
 ActiveRecord::Base.establish_connection({
@@ -10,4 +19,6 @@ ActiveRecord::Base.establish_connection({
   database: 'pokemon_db'
 })
 
-#binding.pry
+
+
+binding.pry
