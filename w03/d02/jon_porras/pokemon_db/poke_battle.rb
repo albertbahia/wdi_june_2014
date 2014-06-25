b0fa9d7fa86e44d1f6ac51dbf42d97822ab10d48
jonathanporras <jonathanrporras@gmail.com>
2014-06-24 23:39:28 -0400

    Completed Pokemon HW with bonus

--- w03/d02/jon_porras/pokemon_db/poke_battle.rb
+++ w03/d02/jon_porras/pokemon_db/poke_battle.rb
@@ -19,5 +19,26 @@ trainer2.pokemons = []
 trainer1.choose_pokemon(Pokemon.get_pokemon)
 trainer2.choose_pokemon(Pokemon.get_pokemon)
 
-trainer1.pokemons.map {|pokemon| pokemon.total_attack}
-trainer2.pokemons.map {|pokemon| pokemon.total_attack}
+
+trainer1_high_score = trainer1.pokemons.map { |pokemon| pokemon.total_attack }.max
+trainer2_high_score = trainer2.pokemons.map { |pokemon| pokemon.total_attack }.max
+
+if trainer1_high_score > trainer2_high_score
+  top = trainer1_high_score
+    trainer1.pokemons.each do |pokemon|
+      if pokemon.total_attack == top
+        puts("Congratulations, #{pokemon.name} has triumphed!!!!!!!")
+      end
+    end
+else
+  top = trainer2_high_score
+    trainer2.pokemons.each do |pokemon|
+      if pokemon.total_attack == top
+        puts("Congratulations, #{pokemon.name} has triumphed!!!!!!!")
+      end
+  end
+end
+
+
+
+# trainer2.pokemons.map {|pokemon| pokemon.total_attack}
