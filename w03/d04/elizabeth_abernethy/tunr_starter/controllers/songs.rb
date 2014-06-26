6b96a3fea554b23ae088d52f538dfa7b6e579d42
MeetElizabeth <ecaberne@gmail.com>
2014-06-26 17:34:09 -0400

    made progress...

--- w03/d04/elizabeth_abernethy/tunr_starter/controllers/songs.rb
+++ w03/d04/elizabeth_abernethy/tunr_starter/controllers/songs.rb
@@ -1,41 +1,41 @@
 # index
-get "/songs" do
-  @songs = Song.all
+get '/songs' do
+  @song = Song.all
   erb(:"songs/index")
 end
 
 # new
-get "/songs/new" do
+get '/songs/new' do
   erb(:"songs/new")
 end
 
 # create
-post "/songs" do
+post '/songs' do
   new_song = Song.create(params[:song])
   redirect("/songs/#{new_song.id}")
 end
 
 #show
-get "/songs/:id" do
-  @songs = Song.find(params[:id])
+get '/songs/:id' do
+  @song = Song.find(params[:id])
   erb(:"songs/show")
 end
 
 # edit
-get "/songs/:id/edit" do
-  @songs = Song.find(params[:id])
-  erb(:"songs/#{song.id}/edit")
+get '/songs/:id/edit' do
+  @song = Song.find(params[:id])
+  erb(:"songs/edit")
 end
 
 # update
-put "/songs/:id" do
+put '/songs/:id' do
   song = Song.find(params[:id])
   song.update(params[:song])
   redirect("/songs/#{song.id}")
 end
 
 # destroy
-post "/songs/:id/delete" do
+post '/songs/:id/delete' do
   song = Song.find(params[:id])
   song.destroy
   redirect('/songs')
