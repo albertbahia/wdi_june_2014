05724384fe055172ebcc56ec4c844eae37ea9d63
MeetElizabeth <ecaberne@gmail.com>
2014-06-26 14:27:02 -0400

    added tunr_starter folder

---
+++ w03/d04/elizabeth_abernethy/controllers/songs.rb
@@ -0,0 +1,27 @@
+# index
+get "/songs" do
+end
+
+# new
+get "/songs/new" do
+end
+
+# create
+post "/songs" do
+end
+
+#show
+get "/songs/:id" do
+end
+
+# edit
+get "/songs/:id/edit" do
+end
+
+# update
+put "/songs/:id" do
+end
+
+# destroy
+post "/songs/:id/delete" do
+end
