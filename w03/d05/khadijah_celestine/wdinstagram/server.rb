6bb2608a6a8e9bf15449398a033086e12d00d542
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-06-28 11:42:53 -0700

    added sinatra

--- w03/d05/khadijah_celestine/wdinstagram/server.rb
+++ w03/d05/khadijah_celestine/wdinstagram/server.rb
@@ -1,6 +1,17 @@
 require 'pry'
 
+require 'sinatra'
+require 'sinatra/reloader'
+
 require_relative './models/image.rb'
+
+after do
+	ActiveRecord::Base.connection.close
+end
+
+get '/' do
+	"working"
+end
 
 
 binding.pry
