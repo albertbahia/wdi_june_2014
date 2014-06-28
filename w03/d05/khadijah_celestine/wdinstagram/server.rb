11dd663c290220d50470a5e4760bea9cfdc89664
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-06-28 13:28:32 -0700

    add show view and route

--- w03/d05/khadijah_celestine/wdinstagram/server.rb
+++ w03/d05/khadijah_celestine/wdinstagram/server.rb
@@ -9,10 +9,17 @@ after do
 	ActiveRecord::Base.connection.close
 end
 
+#index
 get '/' do
 	@images = Image.all
 	erb(:index)
 end
 
+#index
 
 
+#show
+get '/images/:id' do
+	@image = Image.find(params[:id])
+	erb(:show)
+end
