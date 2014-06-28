2f7012b81f875965d0bdaf6850cf5d386f835430
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-06-28 13:43:00 -0700

    added new erb and new and create routes

--- w03/d05/khadijah_celestine/wdinstagram/server.rb
+++ w03/d05/khadijah_celestine/wdinstagram/server.rb
@@ -16,10 +16,25 @@ get '/' do
 end
 
 #index
+get '/images' do
+	@images = Image.all
+	erb(:index)
+end
 
+#new - just show the new page with form 
+get '/images/new' do
+	erb(:new)
+end
 
 #show
 get '/images/:id' do
 	@image = Image.find(params[:id])
 	erb(:show)
+end
+
+#create - the form on the new page will come to this route to create and redirect
+post '/images' do
+	new_img = Image.create(params[:image])
+	#the :image in params refers to the hash that is created when we specified in the new.erb form we wanted the thing to be called image
+	redirect("/images/#{new_img.id}")
 end
