da4cd37bf6b79e8712e72fc126dc73f3068d68c6
JamesCPLiu <jamesc.pliu@gmail.com>
2014-06-29 17:09:41 -0400

    Almost?

--- w03/d05/james_liu/hw_03_05/app.rb
+++ w03/d05/james_liu/hw_03_05/app.rb
@@ -5,4 +5,25 @@ require 'active_record'
 
 
 #load specific routes
-require 'controllers/'
+require_relative 'controllers/images' 
+
+#load models
+require_relative 'models/image'
+
+#load active_record and connect to db
+ActiveRecord::Base.establish_connection({
+    database: 'wdinstagram_db',
+    adapter: 'postgresql'
+  })
+
+#fix sinatra/AR issue
+after do 
+  ActiveRecord::Base.connection.close
+end
+
+#Main route
+get '/' do
+  erb(:home)
+end
+
+binding.pry
---
+++ w03/d05/james_liu/hw_03_05/controllers/images.rb
@@ -0,0 +1,51 @@
+# Your app should have the __Seven Deadly Routes__ routes:
+
+# - An index route that will display all photos.
+# - A show route that displays an individual image.
+# - A new route allow a user to create a new image.
+# - A create route that will save the new entry in the database
+# - An edit route that will allow a user to update an image
+# - An update route that will update the database
+# - A delete route that will remove an image.
+
+
+#index route
+get "/images" do
+  @images = Image.all
+  erb(:"images/index")
+end
+
+#new route
+get "images/new" do
+  erb(:"images/new")
+end
+
+#create route
+post "/images" do
+  @image = Image.create!(params[:image])
+  redirect("/images/#{@image.id}")
+end
+
+#show route
+get "/images/:id" do
+  @image = Image.find(params[:id])
+  erb(:"images/show")
+end
+
+#edit route
+get "/images/:id/edit" do
+  @image = Image.find(params[:id])
+  erb(:"images/edit")
+end
+
+#update route
+put "/images/:id" do
+  @image = Image .find(params[:id])
+  erb(:"images/index")
+end
+
+#delete/destroy route
+post "/images/:id/delete" do
+  @image = Image.find(params[:id])
+  erb(:"images/index")
+end
