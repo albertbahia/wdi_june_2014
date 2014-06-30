fadfcb593c8a0087958405768fc8a66233b9fc16
MeetElizabeth <ecaberne@gmail.com>
2014-06-30 19:41:04 -0400

    finished homework

--- w04/d01/elizabeth_abernethy/hw_w04_d01/twdir_app/db/seeds.rb
+++ w04/d01/elizabeth_abernethy/hw_w04_d01/twdir_app/db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+tweed1 = Tweed.create({
+  author: "@StrangeTweeds",
+  content: "My first Tweed!",
+  image_url: "http://lorempixel.com/400/200"
+  })
+
+tweed2 = Tweed.create({
+  author: "@TweedMaster",
+  content: "Monday morning again...",
+  image_url: "http://lorempixel.com/200/400"
+  })
+
+tweed3 = Tweed.create({
+  author: "@PlaidORTweed",
+  content: "Reported documents detailing the massive Tweed emotion study of 2012 available now.",
+  image_url: ""
+  })
+
+tweed4 = Tweed.create({
+  author: "@TweedlyDum",
+  content: "How many of you feel like this today?",
+  image_url: "http://lorempixel.com/250/250"
+  })
+
+tweed5 = Tweed.create({
+  author: "@TweedTweed",
+  content: "SO IN LOVE WITH THIS!",
+  image_url: ""
+  })
+
+tweed6 = Tweed.create({
+  author: "@Tweeeeeeeed",
+  content: "BREAKING NEWS: THIS HAPPENED.",
+  image_url: "http://lorempixel.com/400/400"
+  })
+
+tweed7 = Tweed.create({
+  author: "@Tweed4885",
+  content: "How to increase your productivity at work.",
+  image_url: "http://lorempixel.com/100/250"
+  })
+
+tweed8 = Tweed.create({
+  author: "@SuperTweed",
+  content: "48% of Americans have dogs",
+  image_url: ""
+  })
+
+tweed9 = Tweed.create({
+  author: "@PerpetualTweed",
+  content: "Who created the internet? Find out here!",
+  image_url: ""
+  })
+
+tweed10 = Tweed.create({
+  author: "@Tweedimensional",
+  content: "",
+  image_url: "http://lorempixel.com/350/200"
+  })
+
+tweed1 = Tweed.create({
+  author: "@Guarantweed",
+  content: "",
+  image_url: ""
+  })
