b5617f8436f3da8cbee736e70931d9918327659c
JamesCPLiu <jamesc.pliu@gmail.com>
2014-06-30 19:12:48 -0400

    Step 4

--- w04/d01/james_liu/twdir_app/db/seeds.rb
+++ w04/d01/james_liu/twdir_app/db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+    "tweed1"  => {
+                    author: "John",
+                    content: "Holler",
+                    image_url: "http://giphy.com/gifs/AMJnTys8hKh4A",
+                    timestamp: '2004-09-15 02:02:02'
+                  },
+    "tweed2"  => {
+                    author: "Jacob",
+                    content: "At",
+                    image_url: "http://giphy.com/gifs/sbB6FG3SwxN9m",
+                    timestamp: '2004-10-09 11:11:11'
+                  },
+    "tweed3"  => {       
+                    author: "Jingle",
+                    content: "your",
+                    image_url: "http://giphy.com/gifs/lT0rHNWgsgSJ2",
+                    timestamp: '2004-10-01 12:00:00'
+                  },
+    "tweed4"  => {
+                    author: "Heimer",
+                    content: "....",
+                    image_url: "http://giphy.com/gifs/10E01nJaUZR43e",
+                    timestamp: '2004-10-31 01:01:01'
+                  },
+    "tweed5"  => {
+                    author: "Smith",
+                    content: "I'm hot like fire",
+                    image_url: "http://giphy.com/gifs/VziBPayjUpwf6",
+                    timestamp: '2004-10-23 04:04:04'
+                  }
+    "tweed6"  => {
+                    author: "Mary",
+                    content: "My tWDIr gets hits like Bieber",
+                    image_url: "http://giphy.com/gifs/HAS2zKAMG0cWA",
+                    timestamp: '2004-10-01 12:15:12'
+                  },
+    "tweed7"  => {
+                    author: "Kate",
+                    content: "Soup",
+                    image_url: "http://giphy.com/gifs/xCFtijAk1urio",
+                    timestamp: '2004-11-13 11:10:01'
+                  },
+    "tweed8"  => {
+                    author: "And",
+                    content: "sandwich",
+                    image_url: "http://giphy.com/gifs/lV506aI9fTh6M",
+                    timestamp: '2004-10-30 05:57:23'
+                  },
+    "tweed9"  => {
+                    author: "Ashley",
+                    content: "sushi",
+                    image_url: "http://giphy.com/gifs/XyD8xSueMQJK8",
+                    timestamp: '2004-10-15 09:14:00'
+                  },
+    "tweed10" => {
+                    author: "Olsen",
+                    content: "lobster",
+                    image_url: "http://giphy.com/gifs/GD5MSJes52mg8",
+                    timestamp: '2004-10-20 10:30:54'
+                  }
