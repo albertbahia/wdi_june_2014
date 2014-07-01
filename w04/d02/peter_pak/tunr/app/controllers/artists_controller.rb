dcdd7864e3ab66d80b748c26ec91a53628200c19
ppakdev <ppak73@gmail.com>
2014-07-01 13:39:41 -0400

    morning stuff

---
+++ w04/d02/peter_pak/tunr/app/controllers/artists_controller.rb
@@ -0,0 +1,40 @@
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+  end
+
+  def search
+    search_term = params[:search]
+    @api_call = Itunes.artist_search(search_term)
+  end
+
+  def create
+    @artist = Artist.create!(params[:artist].permit!)
+    redirect_to(artist_path(@artist))
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(params[:artist].permit!)
+    redirect_to(artist_path(@artist))
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to(artists_path)
+  end
+
+end
