9c48cdbe311120a99b573b31bd532add53a14cc2
TaliaS1214 <talias432@gmail.com>
2014-06-30 21:53:28 -0400

    Completed Assignment

---
+++ w04/d01/sean_talia/twdir_app/db/seeds.rb
@@ -0,0 +1,61 @@
+Tweed.destroy_all
+
+Tweed.create!({
+  author: "Sean",
+  content: "Who's this guy?",
+  image_url: "http://nerdycow.com/wp-content/uploads/2012/11/man-in-white-dress-interview-1_3.jpg"
+  })
+
+Tweed.create!({
+  author: "Dave",
+  content: "Omg so hungry 4 pizza rite nowwww",
+  image_url: "https://media.giphy.com/media/LTnGKg9PSE3ZK/giphy.gif"
+  })
+
+Tweed.create!({
+  author: "Sean",
+  content: "Baseball season rulez",
+  image_url: "http://outsidepitchmlb.files.wordpress.com/2014/04/cabrera-front.jpg"
+  })
+
+Tweed.create!({
+  author: "Michaela",
+  content: "So tired frum WDI, need 2 sleep",
+  image_url: ""
+  })
+
+Tweed.create!({
+  author: "Lauren",
+  content: "Saw this dude in Union Square...HILARIOUS.",
+  image_url: "http://i1.ytimg.com/vi/YlmJCgziGpM/maxresdefault.jpg"
+  })
+
+Tweed.create!({
+  author: "Lauren",
+  content: "Here comes that NYC summer heat :(",
+  image_url: "http://bygonebureau.com/wp-content/uploads/2013/09/facesweat.jpg"
+  })
+
+Tweed.create!({
+  author: "Sean",
+  content: "Best mario game ever amiritetho?",
+  image_url: "http://tieattack.files.wordpress.com/2013/03/super_mario_bros_3_by_ggrock70-d36fqni.png"
+  })
+
+Tweed.create!({
+  author: "Dave",
+  content: "Anyone else on tWDIr a G.G. Allin fan?",
+  image_url: ""
+  })
+
+Tweed.create!({
+  author: "Michaela",
+  content: "Rick Ross <3 his boobs",
+  image_url: "http://dmpmnxvfdwr3.cloudfront.net/wp-content/uploads/2012/08/tumblr_m8tb0rhSG11qa42jro1_1280.jpg"
+  })
+
+Tweed.create!({
+  author: "Dave",
+  content: "Fargo > GoT",
+  image_url: "http://geeknation.com/wp-content/uploads/fargo_tv_on_fx.0_cinema_1200.0.png"
+  })
