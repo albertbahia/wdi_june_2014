f75b246e1ac4b05a10d7827a22c4a78a7dc4d01a
MeetElizabeth <ecaberne@gmail.com>
2014-07-02 21:06:19 -0400

    step 1, 2, 3 completed: data modeling, database creation, models and seeds, validations

---
+++ w04/d03/elizabeth_abernethy/hw_w04_d03/movietrailer_app/app/models/movie.rb
@@ -0,0 +1,15 @@
+class Movie < ActiveRecord::Base
+  has_many :actors
+
+  # Movie should have a title
+  validates(:title, { presence: true })
+
+  # Movie should have a plot
+  validates(:plot, { presence: true })
+
+  # Movie year should be a number / integer / greater than 0
+  validates(:year, { numericality: { only_integer: true, greater_than: 1800 } })
+
+  # Movie title should not exceed 50 characters
+  validates(:title, {length: { maximum: 50 } })
+end
