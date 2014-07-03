4d22eba9efd4d31205b64648e77c3f201d9a1d9e
jonathanporras <jonathanrporras@gmail.com>
2014-07-03 08:05:37 -0400

    in class work

--- w04/d01/INSTRUCTOR/in-class/tunr/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get  "artists"             => "artists#index"
-  get  "artists/new"         => "artists#new"
-  post "artists"             => "artists#create"
-  get  "artists/:id"         => "artists#show"
-  get  "artists/:id/edit"    => "artists#edit"
-  post "artists/:id"         => "artists#update"
-  post "artists/:id/delete"  => "artists#destroy"
-end
