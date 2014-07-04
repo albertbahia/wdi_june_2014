81efaf93c95fb0df2492541df54460f0a0dd0151
hoanewton <caohoa86hp@gmail.com>
2014-07-04 10:09:32 -0400

    make OMDB.search work

--- w04/d03/hoa_newton/movie_trailer/app/models/omdb.rb
+++ w04/d03/hoa_newton/movie_trailer/app/models/omdb.rb
@@ -1,16 +1,8 @@
 class OMDB
 	def self.search(term)
-		search_url = URI.escape("http://ww.omdbapi.com/?s=#{term}")
+		search_url = URI.escape("http://omdbapi.com/?s=#{term}")
 		api_response = HTTParty.get(search_url)  #string
-
-		results = JSON.parse(api_response)["search"]
-
-		results_array = []
-		results.each do |omdb_id|
-			omdb_id
-			#make an api call
-			#store the result in my results_array
-		end
+		results = JSON.parse(api_response)["Search"]
 	end
 end
 
