6210a4325ae389135a128710e7f9ccacd6b9bd83
MeetElizabeth <ecaberne@gmail.com>
2014-07-04 15:19:21 -0400

    seeded database with 8 groups, 32 teams, and 24 players

--- w04/d04/elizabeth_abernethy/hw_w04_d04/worldcupfevr_app/db/seeds.rb
+++ w04/d04/elizabeth_abernethy/hw_w04_d04/worldcupfevr_app/db/seeds.rb
@@ -1,7 +1,425 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# -------------- GROUPS -------------------
+
+Group.create({
+  name: 'Group A'
+  });
+
+Group.create({
+  name: 'Group B'
+  });
+
+Group.create({
+  name: 'Group C'
+  });
+
+Group.create({
+  name: 'Group D'
+  });
+
+Group.create({
+  name: 'Group E'
+  });
+
+Group.create({
+  name: 'Group F'
+  });
+
+Group.create({
+  name: 'Group G'
+  });
+
+Group.create({
+  name: 'Group H'
+  });
+
+# -------------- TEAMS -------------------
+
+
+  Team.create({
+    country: 'Brazil',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/1000px-Flag_of_Brazil.svg.png',
+    manager_name: 'Luiz Felipe Scolari',
+    group_id: 1
+  });
+  Team.create({
+    country: 'Mexico',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Mexico_(reverse).png',
+    manager_name: 'Rafael Marquez',
+    group_id: 1
+  });
+  Team.create({
+    country: 'Croatia',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/1200px-Flag_of_Croatia.svg.png',
+    manager_name: 'Niko Kovač',
+    group_id: 1
+  });
+  Team.create({
+    country: 'Cameroon',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Flag_of_Cameroon.svg/600px-Flag_of_Cameroon.svg.png',
+    manager_name: 'Volker Finke',
+    group_id: 1
+  });
+
+
+  Team.create({
+    country: 'Netherlands',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg',
+    manager_name: 'Louis van Gaal',
+    group_id: 2
+  });
+  Team.create({
+    country: 'Chile',
+    flag_url: 'http://www.mapsnworld.com/chile/chile-flag.jpg',
+    manager_name: 'Jorge Sampaoli',
+    group_id: 2
+  });
+  Team.create({
+    country: 'Spain',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/750px-Flag_of_Spain.svg.png',
+    manager_name: 'Vincent Del Bosque',
+    group_id: 2
+  });
+  Team.create({
+    country: 'Australia',
+    flag_url: 'http://www.flagsaustralia.com.au/images/Australia-1908-(blue)l.jpg',
+    manager_name: 'Ange Postecoglou',
+    group_id: 2
+  });
+
+
+  Team.create({
+    country: 'Colombia',
+    flag_url: 'http://worldatlas.com/webimage/flags/countrys/zzzflags/colarge.gif',
+    manager_name: 'Jose Pekerman',
+    group_id: 3
+  });
+  Team.create({
+    country: 'Greece',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/600px-Flag_of_Greece.svg.png',
+    manager_name: 'Fernando Santos',
+    group_id: 3
+  });
+  Team.create({
+    country: "Côte D'ivoire",
+    flag_url: 'https://www.wholeplanetfoundation.org/wp-content/uploads/2014/01/ivorycoast-966x500.png',
+    manager_name: 'Sabri Lamouchi',
+    group_id: 3
+  });
+  Team.create({
+    country: 'Japan',
+    flag_url: 'http://www.travelandtourworld.com/wp-content/uploads/2013/08/japan-flag.gif',
+    manager_name: 'Alberto Zaccheroni',
+    group_id: 3
+  });
+
+  Team.create({
+    country: 'Costa Rica',
+    flag_url: 'http://costarica.com/resize_image?path=/dotAsset/c500d649-0599-42cb-9bfe-ecdd884a51d6.jpg&w=650&h=430',
+    manager_name: 'Jorge Luis Pinto',
+    group_id: 4
+  });
+  Team.create({country: 'Uruguay',
+    flag_url: 'http://www.mapshop.com/Flags/Flag-Images/urug.png',
+    manager_name: 'Oscar Tabarez',
+    group_id: 4
+  });
+  Team.create({country: 'Italy',
+    flag_url: 'http://www.indexmundi.com/flags/it-lgflag.gif',
+    manager_name: 'Cesare Prandelli',
+    group_id: 4
+  });
+  Team.create({country: 'England',
+    flag_url: 'http://www.picturesnew.com/media/images/england-flag-photo1.jpg',
+    manager_name: 'Roy Hodgson',
+    group_id: 4
+  });
+
+
+  Team.create({
+    country: 'France',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Civil_and_Naval_Ensign_of_France.svg/600px-Civil_and_Naval_Ensign_of_France.svg.png',
+    manager_name: 'Didier Deschamps',
+    group_id: 5
+  });
+  Team.create({
+    country: 'Switzerland',
+    flag_url: 'http://www.dispaxworld2014.com/wp-content/uploads/2013/09/swiss_flag.gif',
+    manager_name: 'Ottmar Hitzfeld',
+    group_id: 5
+  });
+  Team.create({
+    country: 'Ecuador',
+    flag_url: 'http://www.sciencekids.co.nz/images/pictures/flags680/Ecuador.jpg',
+    manager_name: 'Reinaldo Rueda',
+    group_id: 5
+  });
+  Team.create({
+    country: 'Honduras',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Flag_of_Honduras.svg/1000px-Flag_of_Honduras.svg.png',
+    manager_name: 'Luis Fernando Suarez',
+    group_id: 5
+  });
+
+
+  Team.create({
+    country: 'Argentina',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/en/4/4d/Flag_of_Argentina_(bordered).png',
+    manager_name: 'Alejandro Sabella',
+    group_id: 6
+  });
+  Team.create({
+    country: 'Nigeria',
+    flag_url: 'http://worldatlas.com/webimage/flags/countrys/zzzflags/nglarge.gif',
+    manager_name: 'Stephen Keshi',
+    group_id: 6
+  });
+  Team.create({
+    country: 'Bosnia and Herzegovina',
+    flag_url: 'http://www.mapsnworld.com/bosnia-and-herzegovina/bosnia-herzegovina-flag.jpg',
+    manager_name: 'Safet Susic',
+    group_id: 6
+  });
+  Team.create({
+    country: 'Iran',
+    flag_url: 'http://violentdeathproject.com/images/iran-flag.gif',
+    manager_name: 'Carlos Queiroz',
+    group_id: 6
+  });
+
+  Team.create({
+    country: 'Germany',
+    flag_url: 'http://www.german-flag.org/german-640.gif',
+    manager_name: 'Joachim Loew',
+    group_id: 7
+  });
+  Team.create({
+    country: 'USA',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/640px-Flag_of_the_United_States.svg.png',
+    manager_name: 'Jurgen Klinsmann',
+    group_id: 7
+  });
+  Team.create({
+    country: 'Portugal',
+    flag_url: 'http://www.theflagshop.co.uk/ekmps/shops/speed/images/portugal-flag-194-p.jpg',
+    manager_name: 'Paulo Bento',
+    group_id: 7
+  });
+  Team.create({
+    country: 'Ghana',
+    flag_url: 'http://cdn.ghanaweb.com/GHP/img/pics/79307655.jpg',
+    manager_name: 'Akwasi Appiah',
+    group_id: 7
+  });
+
+
+  Team.create({
+    country: 'Belgium',
+    flag_url: 'http://mapsofworld.himmera.com/flags/Belgium_Flag_big.jpg',
+    manager_name: 'Marc Wilmots',
+    group_id: 8
+  });
+  Team.create({
+    country: 'Algeria',
+    flag_url: 'http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_Algeria.svg/900px-Flag_of_Algeria.svg.png',
+    manager_name: 'Vahid Hililhodzic',
+    group_id: 8
+  });
+  Team.create({
+    country: 'Russia',
+    flag_url: 'http://russia.adoption.com/uni/cms/Image/international/flag-russia.gif',
+    manager_name: 'Fabio Capello',
+    group_id: 8
+  });
+  Team.create({
+    country: 'Korea Republic',
+    flag_url: 'http://blog.zerohungerchallenge.org/wp-content/uploads/2014/05/republic-of-korea-flag.gif',
+    manager_name: 'Hong-Myung Bo',
+    group_id: 8
+  });
+
+
+# -------------- PLAYERS -------------------
+
+#USA
+Player.create({
+  name: 'Tim Howard',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/178420.png',
+  position: 'Goalkeeper',
+  skill_level: 78,
+  team_id: 26
+  });
+Player.create({
+  name: 'Jermaine Jones',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/183893.png',
+  position: 'Midfielder',
+  skill_level: 76,
+  team_id: 26
+  });
+Player.create({
+  name: 'Clint Dempsey',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/207851.png',
+  position: 'Forward',
+  skill_level: 83,
+  team_id: 26
+  });
+
+# Germany
+Player.create({
+  name: 'Manual Neuer',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/228912.png',
+  position: 'Goalkeeper',
+  skill_level: 84,
+  team_id: 25
+  });
+Player.create({
+  name: 'Philipp Lahm',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/196748.png',
+  position: 'Defender',
+  skill_level: 94,
+  team_id: 25
+  });
+Player.create({
+  name: 'Miroslav Klose',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/182206.png',
+  position: 'Forward',
+  skill_level: 57,
+  team_id: 25
+  });
+
+# Brazil
+Player.create({
+  name: 'Marcelo Vieira',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/218284.png',
+  position: 'Defender',
+  skill_level: 90,
+  team_id: 1
+  });
+Player.create({
+  name: 'Neymar de Silva Santos Júnior',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/314197.png',
+  position: 'Forward',
+  skill_level: 95,
+  team_id: 1
+  });
+Player.create({
+  name: 'Anderson Hernandes de Carvalho Viana Lima',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/239424.png',
+  position: 'Midfielder',
+  skill_level: 41,
+  team_id: 1
+  });
+
+# France
+Player.create({
+  name: 'Mathieu Debuchy',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/358011.png',
+  position: 'Defender',
+  skill_level: 78,
+  team_id: 17
+  });
+Player.create({
+  name: 'Mathieu Valbuena',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/297106.png',
+  position: 'Midfielder',
+  skill_level: 92,
+  team_id: 17
+  });
+Player.create({
+  name: 'Olivier Giroud',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/358015.png',
+  position: 'Forward',
+  skill_level: 74,
+  team_id: 17
+  });
+
+# Italy
+Player.create({
+  name: 'Salvatore Sirigu',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/298655.png',
+  position: 'Goalkeeper',
+  skill_level: 58,
+  team_id: 15
+  });
+Player.create({
+  name: 'Claudio Marchisio',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/298664.png',
+  position: 'Midfielder',
+  skill_level: 64,
+  team_id: 15
+  });
+Player.create({
+  name: 'Antonio Cassano',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/177927.png',
+  position: 'Forward',
+  skill_level: 49,
+  team_id: 15
+  });
+
+# England
+Player.create({
+  name: 'Wayne Rooney',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/196842.png',
+  position: 'Forward',
+  skill_level: 77,
+  team_id: 16
+  });
+Player.create({
+  name: 'Steven Gerrard',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/214961.png',
+  position: 'Midfielder',
+  skill_level: 78,
+  team_id: 16
+  });
+Player.create({
+  name: 'Jordan Henderson',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/356189.png',
+  position: 'Midfielder',
+  skill_level: 65,
+  team_id: 16
+  });
+
+# Belgium
+Player.create({
+  name: 'Nicolas Lombaerts',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/216874.png',
+  position: 'Defender',
+  skill_level: 41,
+  team_id: 29
+  });
+Player.create({
+  name: 'Dries Mertens',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/358114.png',
+  position: 'Midfielder',
+  skill_level: 87,
+  team_id: 29
+  });
+Player.create({
+  name: 'Romelu Lukaku',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/358112.png',
+  position: 'Forward',
+  skill_level: 78,
+  team_id: 29
+  });
+
+# Costa Rica
+Player.create({
+  name: 'Joel Campbell',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/307529.png',
+  position: 'Forward',
+  skill_level: 73,
+  team_id: 13
+  });
+Player.create({
+  name: 'Johnny Acosta',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/339447.png',
+  position: 'Defender',
+  skill_level: 41,
+  team_id: 13
+  });
+Player.create({
+  name: 'Junior Diaz',
+  photo_url: 'http://img.fifa.com/images/fwc/2014/players/prt-3/209940.png',
+  position: 'Defender',
+  skill_level: 77,
+  team_id: 13
+  });
