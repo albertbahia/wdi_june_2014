69d63238602dc33dbbe1130bb19403e638b734fc
DrRobotmck <mckenneth.scott@icloud.com>
2014-07-06 18:28:07 -0400

    update movietrailer

---
+++ w04/d04/ASSIGNMENT_FILES/movietrailer_app/db/migrate/20140703000703_create_trailers.rb
@@ -0,0 +1,11 @@
+class CreateTrailers < ActiveRecord::Migration
+  def change
+    create_table :trailers do |t|
+      t.string :title
+      t.string :embed_url
+      t.belongs_to :movie
+
+      t.timestamps
+    end
+  end
+end
