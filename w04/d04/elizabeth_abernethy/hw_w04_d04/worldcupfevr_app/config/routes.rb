0f4725e4daf7d3cdd4c76d7b4e37a33326f2e5fd
MeetElizabeth <ecaberne@gmail.com>
2014-07-06 22:23:23 -0400

    finished homework assignment

--- w04/d04/elizabeth_abernethy/hw_w04_d04/worldcupfevr_app/config/routes.rb
+++ w04/d04/elizabeth_abernethy/hw_w04_d04/worldcupfevr_app/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get '/'                 =>   'welcome#index', as: 'root'
-  get '/about'            =>   'welcome#about', as: 'about'
+  get "/"                 =>   'welcome#index', as: 'root'
+  get "/about"            =>   'welcome#about', as: 'about'
 
   resources :groups, :teams, :players
-  
+
 end
