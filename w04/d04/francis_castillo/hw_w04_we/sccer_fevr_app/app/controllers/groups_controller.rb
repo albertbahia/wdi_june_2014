b684ed1f1fac4fdb7b742a5b621bd3aca75b4e49
fgc29 <fgc0077@gmail.com>
2014-07-07 00:07:41 -0400

    completed assignment

--- w04/d04/francis_castillo/hw_w04_we/sccer_fevr_app/app/controllers/groups_controller.rb
+++ w04/d04/francis_castillo/hw_w04_we/sccer_fevr_app/app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to(group_path(@group))
+      redirect_to(group_path( @group ))
     else
       render :new
     end
