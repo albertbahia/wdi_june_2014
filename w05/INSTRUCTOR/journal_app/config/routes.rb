c8629177137932721305fa7539ae69aff0019a23
inmakn <themousepolice@gmail.com>
2014-07-07 15:13:22 -0400

    trying to pull

--- w05/INSTRUCTOR/journal_app/config/routes.rb
+++ w05/INSTRUCTOR/journal_app/config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  # Entries
+  get '/entries' => 'entries#index'
+  get '/entries/new' => 'entries#new', as: 'new_entry'
+  post '/entries' => 'entries#create'
 end
