8696a9bb2fa3ada76976724f0322f64a1f639460
Hari Mohanraj <hari.mohanraj89@gmail.com>
2014-07-17 10:16:04 -0400

    Update apartment_spec.rb

--- w02/d05/ASSIGNMENT_FILES/weekend_assignment/spec/apartment_spec.rb
+++ w02/d05/ASSIGNMENT_FILES/weekend_assignment/spec/apartment_spec.rb
@@ -8,79 +8,53 @@ describe Apartment do
 
   it 'has a name' do
     expect(apartment.name).to_not be_nil
-    expect(apartment.name).to eq("1A")
   end
 
   it 'has a floor' do
     expect(apartment.floor).to_not be_nil
-    expect(apartment.floor).to eq(1)
 
   end
 
   it 'has a price' do
     expect(apartment.price).to_not be_nil
-    expect(apartment.price).to eq(1200)
 
   end
 
   it 'has a sqftage' do
     expect(apartment.sqft).to_not be_nil
-    expect(apartment.sqft).to eq(2000)
 
   end
 
   it 'has bedrooms' do
     expect(apartment.bedrooms).to_not be_nil
-    expect(apartment.bedrooms).to eq(2)
 
   end
 
   it 'has bathrooms' do
     expect(apartment.bathrooms).to_not be_nil
-    expect(apartment.bathrooms).to eq(2)
 
   end
 
   it 'has tenants' do
     expect(apartment.tenants).to_not be_nil
-    expect(apartment.tenants).to eq([])
 
   end
 
   describe '#info' do
-    it 'lists the apartment info' do
-      expect(apartment.info).to include(apartment.name)
-      expect(apartment.info).to include(apartment.floor.to_s)
-      expect(apartment.info).to include(apartment.price.to_s)
-      expect(apartment.info).to include(apartment.sqft.to_s)
-      expect(apartment.info).to include(apartment.bedrooms.to_s)
-      expect(apartment.info).to include(apartment.bathrooms.to_s)
+    xit 'lists the apartment info' do
     end
   end
 
   describe '#list_tenants' do
-    it 'lists all of the tenants information' do
-      apartment.add_tenants(tenant)
-      apartment.add_tenants(tenant_2)
-      expect(apartment.list_tenants).to eq("Name: Joe, Age: 25, Gender: Male\nName: Jorb, Age: 9000, Gender: Female")
+    xit 'lists all of the tenants information' do
     end
 
   end
 
   describe '#add_tenant' do
-    it 'adds a tenant to the apartment' do
-      apartment.add_tenants(tenant)
-      apartment.add_tenants(tenant_2)
-      apartment.add_tenants(tenant_3)
-      expect(apartment.tenants).to eq([tenant, tenant_2])
+    xit 'adds a tenant to the apartment' do
     end
-    it 'does not add more tenants than rooms' do
-      tester = Tenant.new("Testguy", 32, "Male")
-      apartment.add_tenants(tenant)
-      apartment.add_tenants(tenant_2)
-      apartment.add_tenants(tenant_3)
-      apartment.add_tenants(tester)
-      expect(apartment.tenants).to eq([tenant, tenant_2])
+    xit 'does not add more tenants than rooms' do
     end
   end
 
