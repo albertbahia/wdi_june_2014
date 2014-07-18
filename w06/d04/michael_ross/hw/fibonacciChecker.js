a6b1157f58581322ad45050cabd6cf383beff462
MALRMALR <michaelalross@gmail.com>
2014-07-18 00:06:31 -0400

    completed assignment

--- w06/d04/michael_ross/hw/fibonacciChecker.js
+++ w06/d04/michael_ross/hw/fibonacciChecker.js
@@ -1,43 +1,15 @@
-# def sum_of_even_fibonaccis(n)
-#   return n if n <= 1
-#   sum_of_even_fibonaccis(n-1)+ sum_of_even_fibonaccis(n-2)
-# end
-
-def sum_of_even_fibonaccis(limit)
-
-  holder = [1,2]
-
-  while ( holder[-1] + holder[-2] ) < limit
-    holder << holder[-1] + holder[-2]
-  end
-
-  return holder.select { |n| n.even? }.reduce(:+)
-
-end
-
-def sum_of_even_fibonacci(limit)
-    num = 1
-    adder = 2
-    holder = 0
-    total = 0
-    while adder < limit
-      total += adder if holder.even?
-      holder = num + adder
-      num = adder
-      adder = holder
-      return total if adder > limit
-    end
-    return total
-end
-
-
 var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-function fibo(n) {
-  var f = [];
-  for (var c = 0; c < n; ++c) {
-    f.push((c < 2) ? c : f[c-1] + f[c-2]);
+function doYouEvenFibonacciBro(num)
+  var f = []
+  for (var c = 0) {
+    while c < num; ++c {
+      f << (c < 2) ? c :f[c - 1] + f[c = - 2]
+    }
+    return f;
+    if num = f
+    return "True"
+  else
+    return "False"
   }
-  return f;
-}
--- w06/d04/michael_ross/hw/scrabble.js
+++ w06/d04/michael_ross/hw/scrabble.js
@@ -1,12 +1,12 @@
 var scrabbleLetterValues = [[],['A','E','I','U','N','R','S','L','O','T'],['G','D'],['C','M','P','B'],['F','H','W','V','Y'],['K'],[],[],['J','X'],[],['Q','Z']];
 var words = ['watermelon','Juicy','Colorful','beautiful','eat','SWAG'];
 
-var scrabbleNumberValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var scrabbleNumValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // W A T E R M E L O N
 // 4 1 1 1 1 3 1 1 1 1
 
 
-var letter_value = function(letter) {
-  for(var i = 0)
+var scrabbleNumValues = function(letter) {
+  for (var i = 0; i <scrabbleLetterValues.length; )
 }
