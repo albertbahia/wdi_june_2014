7c54c7b1ce9d6b46296dddd6d4d6a47cb838eacd
MALRMALR <michaelalross@gmail.com>
2014-07-17 20:26:48 -0400

    added namesake section of hw

---
+++ w06/d04/michael_ross/hw/namesake.rb
@@ -0,0 +1,56 @@
+numbers = [1,2,3,4,5,6]
+strings = ['Hi','my','name','is', 'Bob']
+duplicate_strings = ['yo', 'what', 'is', 'up', 'what']
+
+# Index
+
+def find_index(string)
+  i = 0
+  while i < array.length do
+    index = yield array[i]
+  end
+    puts index
+end
+
+# MAP
+
+def my_map(array)
+  i = 0
+  while i < array.length do
+    new_array = yield array[i]
+    i += 1
+  end
+  puts new_array
+end
+
+array = [1,2,3,4,5,6]
+
+my_map(array) do |num|
+  puts num + 5
+end
+
+# SELECT
+
+def my_select
+  new_array = []
+  self.each do |x|
+    new_array << x if yield(x)
+  end
+  new_array
+end
+
+numbers.my_select { |x| x % 2 == 0 }
+
+# REJECT
+
+def my_reject
+  new_array = []
+  self.each do |x|
+    new_array << x unless yield(x)
+  end
+  new_array
+end
+
+strings = ['Hi','my','name','is', 'Bob']
+
+strings.reject { |x| x.length > 2 }
