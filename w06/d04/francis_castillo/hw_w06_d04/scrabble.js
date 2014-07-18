75e380eb43f3397da18bff83cbc63a2af168394b
fgc29 <fgc0077@gmail.com>
2014-07-18 09:24:27 -0400

    pull request submission for completed assignment

--- w06/d04/francis_castillo/hw_w06_d04/scrabble.js
+++ w06/d04/francis_castillo/hw_w06_d04/scrabble.js
@@ -16,15 +16,3 @@ var words_score = [0,0,0,0,0,0];
     }
   }
   console.log(words_score);
-
-var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 89, 100];
-var numArr = numArr.sort();
-var fibNums = [0,1];
-  for(y=2; y < Math.max.apply(null, numArr); y++) {
-    fibNums.push(fibNums[y-2] + fibNums[y-1]);
-  }
-  for(x=0; x < numArr.length; x++) {
-    if(fibNums.indexOf(numArr[x]) != -1) {
-        console.log(numArr[x] + " " + "is a fibnocci number!");
-    }
-  }
