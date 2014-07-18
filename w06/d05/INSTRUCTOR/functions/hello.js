28248bf2124bf488d170008e76d0bba3aabb2351
harimohanraj89 <hari.mohanraj89@gmail.com>
2014-07-18 11:59:29 -0400

    passing functions around in class

--- w06/d05/INSTRUCTOR/functions/hello.js
+++ w06/d05/INSTRUCTOR/functions/hello.js
@@ -1,12 +1,13 @@
 // Named function
-// function hello(message) {
-//   console.log("Hi there. " + message);
-// }
-//
-// hello("This is jackass.");
-// hello("This is sparta.");
-// hello("This is dog.");
+function hello(message) {
+  console.log("Hi there. " + message);
+}
 
+hello("This is jackass.");
+hello("This is sparta.");
+hello("This is dog.");
+
+// Anonymous function stored in a variable
 var add = function(a, b) {
   return a + b;
 };
