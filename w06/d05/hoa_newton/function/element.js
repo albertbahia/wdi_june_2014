e7d5c77546b3dafa8758528e710de28d3e48a3c4
hoanewton <caohoa86hp@gmail.com>
2014-07-18 16:31:52 -0400

    finish JS lab

---
+++ w06/d05/hoa_newton/function/element.js
@@ -0,0 +1,8 @@
+function elements(array){
+	for (var i = 0; i < array.length; i++){
+		console.log(array[i]);
+	}
+}
+
+elements([1, 2, 'Hello'])
+elements([1, "Dog", 'Hello'])
---
+++ w06/d05/hoa_newton/function/scope.js
@@ -0,0 +1,14 @@
+var each = function(fxn, arr) {
+	for (var i = 0; i < arr.length; i++ ){
+		fxn(arr[i]);
+	}
+};
+
+// var element = function(el){
+// 	console.log(el);
+// };
+
+// var my_array = [1, 2, "hello"];
+// each(element, my_array);
+// each(element, ["dog", 'cat', 13]);
+
