f2c6340a463febb752233bbe16a83b5127bb0b96
MALRMALR <michaelalross@gmail.com>
2014-07-18 12:30:30 -0400

    updated hw

--- w06/d04/michael_ross/hw/fibonacciChecker.js
+++ w06/d04/michael_ross/hw/fibonacciChecker.js
@@ -1,15 +1,13 @@
-var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
-function doYouEvenFibonacciBro(num)
-  var f = []
-  for (var c = 0) {
-    while c < num; ++c {
-      f << (c < 2) ? c :f[c - 1] + f[c = - 2]
-    }
-    return f;
-    if num = f
-    return "True"
-  else
-    return "False"
+for (var i = 0; i < numbers.length; i++) {
+  var firstCheck = Math.sqrt( 5 * Math.pow(numbers[i],2) - 4);
+  var secondCheck = Math.sqrt( 5 * Math.pow(numbers[i],2) + 4);
+
+  if (firstCheck == parseInt(firstCheck) || secondCheck == parseInt(secondCheck)){
+    console.log('The number ' + numbers[i] ' is a fibonacci number');
+  }  else {
+    console.log('The number ' + numbers[i] ' is not a fibonacci number, son');
   }
+}
--- w06/d04/michael_ross/hw/scrabble.js
+++ w06/d04/michael_ross/hw/scrabble.js
@@ -1,12 +1,21 @@
 var scrabbleLetterValues = [[],['A','E','I','U','N','R','S','L','O','T'],['G','D'],['C','M','P','B'],['F','H','W','V','Y'],['K'],[],[],['J','X'],[],['Q','Z']];
 var words = ['watermelon','Juicy','Colorful','beautiful','eat','SWAG'];
+var finalScores = [];
 
-var scrabbleNumValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-// W A T E R M E L O N
-// 4 1 1 1 1 3 1 1 1 1
-
-
-var scrabbleNumValues = function(letter) {
-  for (var i = 0; i <scrabbleLetterValues.length; )
+for ( var i = 0; i < words.length ; i++ ) {
+  var tiles = words[i].split('');
+  //[w,a,t,e,r,m,e,l,o,n]
+  var count = 0;
+  for ( var j = 0; j < tile.length; j++ ) {
+    var tile = tiles[j].toUpperCase();
+    // tile = 'W'
+    for ( var k = 0; k < scrabbleLetterValues.length; k++ ) {
+      if ( scrabbleLetterValues[k].indexOf( tile ) != -1 ){
+        count += k;
+      }
+    }
+  }
+  finalScores.push(count)
 }
+
+console.log(finalScores)
