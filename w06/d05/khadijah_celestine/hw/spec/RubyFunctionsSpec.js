4a630cb2a18b77541b9027b319bacffbead1ad66
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-19 20:31:49 -0700

    wrote two tests for include

--- w06/d05/khadijah_celestine/hw/spec/RubyFunctionsSpec.js
+++ w06/d05/khadijah_celestine/hw/spec/RubyFunctionsSpec.js
@@ -2,13 +2,23 @@ var add_one = function(num) {
   return ++num;
 };
 
+var arr = [3, 4, 5];
+
 describe('Map', function() {
   it('should return a new array with the new elements via the callback', function() {
     expect(map([1, 2, 3],add_one)).toEqual([2,3,4]);
   });
   it('should not change the original array', function() {
-    arr = [3, 4, 5];
     map(arr,add_one);
     expect(arr).toEqual([3, 4, 5]);
+  });
+});
+
+describe('Include', function() {
+  it('should return true if the value is in the array', function() {
+    expect(include(arr, 3)).toEqual(true);
+  });
+  it('should return false if the value is not in the array', function() {
+    expect(include(arr, 6)).toEqual(false);
   });
 });
