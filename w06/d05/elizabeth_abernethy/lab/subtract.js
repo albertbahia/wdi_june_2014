1925465335b323868f0abe78ce5f992e13b253f2
MeetElizabeth <ecaberne@gmail.com>
2014-07-20 08:38:13 -0400

    finished lab work from Friday: calculator: subtract, multiply, divide, factorial, power

---
+++ w06/d05/elizabeth_abernethy/lab/subtract.js
@@ -0,0 +1,10 @@
+// SUBTRACT
+// - write a function that accepts 2 parameters
+// - return result of performing subtraction on those two
+
+var subtract = function(num1, num2) {
+  return num1 - num2
+};
+
+var subtractAnswer = subtract(10, 5);
+console.log(subtractAnswer);
