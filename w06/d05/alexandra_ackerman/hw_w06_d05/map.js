ec3f40332b1885851378b917644bf0e7c7dcef6a
aaackerman <aaackerman@yahoo.com>
2014-07-20 14:51:32 -0400

    refactored map function

--- w06/d05/alexandra_ackerman/hw_w06_d05/map.js
+++ w06/d05/alexandra_ackerman/hw_w06_d05/map.js
@@ -1,22 +1,38 @@
-var forEach = function (array, action) {
-  for (var i = 0; i < array.length; i++) {
-    action(array[i]);
-  }
-};
+//ATTEMPT ONE
+// var each = function (array, action) {
+//   for (var i = 0; i < array.length; i++) {
+//     action(array[i]);
+//   }
+// };
+
+// var map = function(fxn, array) {
+//   var resfult = []; 
+//   each(array, function(element) {
+//     result.push(fxn(element));
+//   });
+//   return result;
+// }
 
 var map = function(fxn, array) {
   var result = []; 
-  forEach(array, function(element) {
-    result.push(fxn(element));
-  });
+  for (var i = 0; i < array.length; i++) {
+    result.push(fxn(array[i]));
+  }
   return result;
-}
+};
 
 var triple = function (element) {
   return element * 3;
+};
+
+var capitalize = function(val){
+  return val.toUpperCase();
 };
 
 numbers = [1, 2, 3, 4];
 console.log(map(triple, numbers));
 console.log(numbers);
 
+strings = ['a','b','c','d'];
+console.log(map(capitalize, strings));
+console.log(strings);
