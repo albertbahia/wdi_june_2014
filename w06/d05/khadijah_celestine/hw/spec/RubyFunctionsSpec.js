237df5a636a3b7f9b12deb1acaf527b31c8f27d2
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-19 20:15:13 -0700

    added two tests for map function

---
+++ w06/d05/khadijah_celestine/hw/spec/RubyFunctionsSpec.js
@@ -0,0 +1,10 @@
+describe('Map', function() {
+  it('should return a new array with the new elements via the callback', function() {
+    expect(map([1, 2, 3],add_one)).toEqual([2,3,4]);
+  });
+  it('should not change the original array', function() {
+    arr = [3, 4, 5];
+    map(arr,add_one);
+    expect(arr).toEqual([3, 4, 5]);
+  });
+});
