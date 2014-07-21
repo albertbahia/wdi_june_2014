1e400a3caf5b25908a4b3064e1a863164537431c
fgc29 <fgc0077@gmail.com>
2014-07-21 17:58:41 -0400

    done

--- w07/d01/francis_castillo/practice/object.js
+++ w07/d01/francis_castillo/practice/object.js
@@ -25,29 +25,53 @@
 
 /////////////////////////////////////
 
-var monkey = {
-  name: "",
-  species: "Chimpanzee",
-  foodsEaten: []
+// var monkey = {
+//   name: "",
+//   species: "Chimpanzee",
+//   foodsEaten: []
+// }
+// monkey.eatSomething = function(thingAsString){
+//   this.foodsEaten.push(thingAsString);
+// }
+//
+// monkey.introduce = function(){
+//   console.log("Hi, my name is " + this.name + " I'm a " + this.species + " and I've eaten " + this.foodsEaten.join(", ") + " ,today!");
+// }
+//
+// var chetah = monkey;
+// chetah.name = "Chetah";
+// chetah.eatSomething("gummy worms");
+// chetah.species = "orangatan";
+//
+// console.log(chetah);
+// monkey.eatSomething("bananas");
+// monkey.eatSomething("worms");
+// monkey.eatSomething("tree ants");
+//
+// monkey.introduce();
+// console.log(monkey["species"]);
+// console.log(monkey["foodsEaten"]);
+
+function makeObject () {
+  return
 }
-monkey.eatSomething = function(thingAsString){
-  this.foodsEaten.push(thingAsString);
+
+console.log(makeObject());
+
+function Monkey (name,species) {
+  this.name = name;
+  this.species = species;
+  this.foodsEaten = [];
+
+  this.eatSomething = function(thing) {
+    this.foodsEaten.push(thing);
+  };
+  this.introduce = function() {
+    return "Hi, I'm " + this.name + " a " + this.species + " and I've eaten " + this.foodsEaten + " today!";
+  };
 }
 
-monkey.introduce = function(){
-  console.log("Hi, my name is " + this.name + " I'm a " + this.species + " and I've eaten " + this.foodsEaten.join(", ") + " ,today!");
-}
+console.log(Monkey());
 
-var chetah = monkey;
-chetah.name = "Chetah";
-chetah.eatSomething("gummy worms");
-chetah.species = "orangatan";
-
-console.log(chetah);
-monkey.eatSomething("bananas");
-monkey.eatSomething("worms");
-monkey.eatSomething("tree ants");
-
-monkey.introduce();
-console.log(monkey["species"]);
-console.log(monkey["foodsEaten"]);
+var cheetah = new Monkey("Cheetah", "Chimpanzee");
+console.log(cheetah);
