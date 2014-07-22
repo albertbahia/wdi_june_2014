8549aaee717fa1517d982e9b8320aa748529fe8c
MeetElizabeth <ecaberne@gmail.com>
2014-07-22 16:35:39 -0400

    tictactoe exercise in class: up to header and footer

--- w07/d02/elizabeth_abernethy/tictactoe/js/app.js
+++ w07/d02/elizabeth_abernethy/tictactoe/js/app.js
@@ -1,22 +1,36 @@
+var game;
+
 $(function() {
   console.log('JS loaded.');
-
-  // call the function once the page is loaded
-  displayGame();
+  game = new TicTacToe();
+  displayGame(game);
 });
 
-function displayGame() {
+function displayGame(game) {
+  // Find the board
   var board = $('#board');
+
+  // Empty the board
   board.empty();
 
   for (var row = 0; row < 3; row++ ) {
+    // Appending each row
     var rowEL = $('<div>').addClass('row').appendTo(board);
-
     for (var column = 0; column < 3; column++ ) {
-      $('<div>').addClass('square').appendTo(rowEL);
+      // Appending each square
+      var squareEl = $('<div>').addClass('square').appendTo(rowEL);
+      if (game.board[row][column]) {
+        squareEl.text(game.board[row][column]);
+      }
     }
   }
+  var info = $('<div>')
+    .addClass('info')
+    .text("You're up: Player " + game.currentPlayer )
+    .appendTo(board);
 
-
-
+  var header = $('<div>')
+    .addClass('header')
+    .text("Tic Tac Toe")
+    .prependTo(board);
 };
