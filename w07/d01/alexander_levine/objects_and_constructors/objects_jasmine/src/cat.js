358b20faab4c5e5b5b30c8ee5fef5b8bf2aff0de
alevsxm <alexmlevine@gmail.com>
2014-07-21 22:13:56 -0400

    hw first commit

---
+++ w07/d01/alexander_levine/objects_and_constructors/objects_jasmine/src/cat.js
@@ -0,0 +1,17 @@
+function Cat(name, breed, furColor, weight) {
+  this.name = name;
+  this.breed = breed;
+  this.furColor = furColor;
+  this.weight = weight;
+  this.age = age;
+  this.hairballs = [];
+  this.meow = function() {
+    return "Meooow!";
+  };
+  this.judgeHuman = function() {
+    return "Looks at human, stares, stalks away unimpressed.";
+  };
+  this.haveCollarRead = function() {
+    return "Collar has been read"
+  }
+}
