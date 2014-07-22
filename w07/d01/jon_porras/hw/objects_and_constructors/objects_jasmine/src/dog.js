da9d3ed6f04a8588cbea3c7b7038b20a3d6cf4e8
jonathanporras <jonathanrporras@gmail.com>
2014-07-21 20:16:56 -0400

    Completed Assignment

---
+++ w07/d01/jon_porras/hw/objects_and_constructors/objects_jasmine/src/dog.js
@@ -0,0 +1,27 @@
+function Dog(name, breed, furColor, weight, age) {
+  this.name = name;
+  this.breed = breed;
+  this.furColor = furColor;
+  this.weight = weight;
+  this.age = age;
+  this.toys = [];
+
+  this.bark = function() {
+    return "Bow Wow Wow Yippie Yo Yippie Yay";
+  }
+
+  this.readCollar = function() {
+    return "Name: " + this.name + " // " + "Age: " + this.age + " // " + this.breed + " // " + this.weight + "lbs // " + this.furColor.charAt(0).toUpperCase() + this.furColor.slice(1) + " Fur";
+  }
+
+  this.getToy = function(toy) {
+    this.toys.push(toy);
+  }
+
+  this.chewUpToy = function(toy) {
+    chewedToy = this.toys[Math.random()*this.toys.length]
+    index = this.toys.indexOf(chewedToy);
+    this.toys.splice(index, 1);
+    return "/Sorry, I destroyed the/" + chewedToy;
+  }
+}
