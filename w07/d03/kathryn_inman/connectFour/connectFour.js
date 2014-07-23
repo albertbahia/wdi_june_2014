a14a3fa3d17ba3c6f544f34a222a5c8303bbd568
inmakn <themousepolice@gmail.com>
2014-07-23 17:27:32 -0400

    html for board display

---
+++ w07/d03/kathryn_inman/connectFour/connectFour.js
@@ -0,0 +1,20 @@
+$(function() {
+  console.log('Loaded, bro');
+  startGame();
+});
+
+
+
+
+
+
+
+function startGame() {
+  var gamePieces = $('.piece');
+  gamePieces.mouseenter(function(){
+    $(this).addClass('hover')
+  });
+  gamePieces.mouseleave(function(){
+    $(this).removeClass('hover')
+  });
+}
