a94ce5c3f01142942e22c4f84adfacf2b8c6d720
DrRobotmck <mckenneth.scott@icloud.com>
2014-07-23 16:11:13 -0400

    hw

---
+++ w07/d03/ASSIGNMENT_FILES/connectFour/connectFour.js
@@ -0,0 +1,14 @@
+$(function() {
+  console.log('Loaded, bro');
+  startGame();
+});
+
+function startGame() {
+  var gamePieces = $('.piece');
+  gamePieces.mouseenter(function(){
+    $(this).addClass('hover')
+  });
+  gamePieces.mouseleave(function(){
+    $(this).removeClass('hover')
+  });
+}
