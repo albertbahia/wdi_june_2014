057afda5df54227b82e3e35f539cba4d37168581
fgc29 <fgc0077@gmail.com>
2014-07-23 15:08:35 -0400

    v1

---
+++ w07/d03/francis_castillo/trillo/js/app.js
@@ -0,0 +1,36 @@
+$(function() {
+  console.log("Hi");
+  $('.card-list li').click(cardClick);
+  $('.done').click(removeFromDone);
+  $('.todo').click(taskDone);
+  $('#new-card-button').click(addTask);
+});
+
+var cardClick = function() {
+  var cardText = $(this).text();
+  console.log(cardText);
+};
+
+var removeFromDone = function() {
+  $(this).remove();
+};
+
+var taskDone = function() {
+  var task = $(this);
+  task.removeClass().addClass("card done");
+  task.prependTo($('#done-column ul'));
+  task.off();
+  task.click(removeFromDone);
+};
+
+var addTask = function() {
+  var task = $('#new-card-text')
+  var inputVal = task.val();
+  var card = $('<li>');
+  
+  card.text(inputVal).addClass("card todo");
+  card.prependTo($('#todo-column ul'));
+  card.off();
+  card.click(taskDone);
+  task.val('');
+};
