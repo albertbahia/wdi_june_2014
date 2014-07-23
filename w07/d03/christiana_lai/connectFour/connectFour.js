4b50618e02baa9eb51014a1a09ace4ea2848653d
clai1018 <clai1018@gmail.com>
2014-07-23 18:55:17 -0400

    only down below

--- w07/d03/christiana_lai/connectFour/connectFour.js
+++ w07/d03/christiana_lai/connectFour/connectFour.js
@@ -1,30 +1,39 @@
 $(function() {
   console.log('JavaScript loaded >>');
   startGame();
-  $('.piece').on('click', changeColor);
   turn = 0;
+  $('.piece').on('click', makeMove);
 });
 
 function startGame() {
   var gamePieces = $('.piece');
   gamePieces.mouseenter(function(){
-    $(this).addClass('hover')
+    $(this).addClass('hover');
   });
   gamePieces.mouseleave(function(){
-    $(this).removeClass('hover')
+    $(this).removeClass('hover');
   });
 }
 
-var changeColor = function() {
-  var piece = $(this);
+
+var changeColor = function(piece) {
   if (piece.hasClass('red') || piece.hasClass('black')) {
-    // if peice has has already been played, don't do anything.
+    // if piece has has already been played, don't do anything.
   } else {
     if (turn % 2 === 0) {
-      piece.addClass('red');
+      piece.addClass('red taken');
     } else {
-      piece.addClass('black');
+      piece.addClass('black taken');
     }
     turn++;
+  }
+};
+
+var makeMove = function() {
+  var piece = $(this);
+  console.log(piece.index());
+  if (piece.index() === 5) {
+    changeColor(piece);
+  } else {
   }
 };
