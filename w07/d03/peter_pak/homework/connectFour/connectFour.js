44fad76c1e3cacf328fab5a109b72bb25f6dd36a
ppakdev <ppak73@gmail.com>
2014-07-23 19:51:07 -0400

    finished hw i think?

--- w07/d03/peter_pak/homework/connectFour/connectFour.js
+++ w07/d03/peter_pak/homework/connectFour/connectFour.js
@@ -1,32 +1,73 @@
 $(function() {
   console.log('Loaded, bro');
   startGame();
-  $('.piece').on('click', makeEmRed);
+  $('.piece').on('click', makeItStick);
 });
 
-function startGame() {
 
+function startGame() {
 }
 
-var makeEmRed = function() {
-  var piece = $(this);
-  if (piece.attr('class') !== 'piece red' && piece.attr('class') !== 'piece black' ){
+// var validPieces = [];
+// for (var i = 0; i <= 6; i++) {
+//   var column = $('.column').eq(i);
+//   var piece = column.children().last();
+//   $('validPieces').add(piece);
+// }
+
+
+// var makeEmRed = function() {
+//   var piece = $(this);
+//   var nextPiece = piece.parent().children().last();
+//   if (nextPiece.attr('class') === 'piece') {
+//     for (var i = 0; i < 6; i++) {
+//       nextPiece = piece.parent().children().eq(i)
+//       if (nextPiece.attr('class') !== 'piece') {
+//         nextPiece = piece.parent().children().eq(i-1);
+//         break
+//       }
+//     }
+//   }
+//   if ($('.red').length === $('.black').length) {
+//     nextPiece.attr('class', 'piece red');
+//   } else {
+//     nextPiece.attr('class', 'piece black');
+//   }
+// }
+// }
+
+
+function makeItStick(){
+  var currentPiece = $(this);
+  var nextPiece;
+
+  if (currentPiece.parent().children().last().attr('class') === 'piece') {
     if ($('.red').length === $('.black').length) {
-    piece.attr('class', 'piece red');
+      nextPiece = currentPiece.parent().children().last();
+      nextPiece.attr('class', 'piece red');
+      $(this).on('click', makeItStick);
     } else {
-      piece.attr('class', 'piece black');
+      nextPiece = currentPiece;
+      nextPiece.attr('class', 'piece black');
+      $(this).on('click', makeItStick);
+    }
+  } else {
+    for (var i = 0; i <= 5; i++){
+      nextPiece = currentPiece.parent().children().eq(i);
+      if (nextPiece.attr('class') !== 'piece') {
+        nextPiece = currentPiece.parent().children().eq(i-1);
+        break;
+      }
+    }
+    if ($('.red').length === $('.black').length) {
+      nextPiece.attr('class', 'piece red');
+      $(this).on('click', makeItStick);
+    } else {
+      nextPiece.attr('class', 'piece black');
+      $(this).on('click', makeItStick);
     }
   }
-};
+}
 
-
-// var makeEmBlack = function() {
-//   var piece = $(this);
-//   if (piece.css('background-color') !== 'rgb(255,0,0)') && (piece.css('background-color') !== 'rgb(0,0,0)') {
-//     piece.css('background-color', 'black');
-//   }
-//   $('.piece').on('click', makeEmRed);
-// };
 //
 //
-// $('.red').lenght
