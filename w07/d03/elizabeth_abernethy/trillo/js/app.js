1d0bafbdcb42e5df33bde3f8b4f64014d3fe322a
MeetElizabeth <ecaberne@gmail.com>
2014-07-23 14:49:58 -0400

    finished trillo exercise

--- w07/d03/elizabeth_abernethy/trillo/js/app.js
+++ w07/d03/elizabeth_abernethy/trillo/js/app.js
@@ -2,9 +2,43 @@ $(function() {
   console.log("Hi");
 
   $('.card').on('click', cardClick);
+  $('.done').on('click', removeCard);
+  $('.todo').on('click', finishCard);
+  $('#new-card-button').on('click', createCard);
 });
 
 var cardClick = function() {
   var cardText = $(this).text();
   console.log(cardText);
+};
+
+var removeCard = function() {
+  $(this).remove();
+};
+
+var finishCard = function() {
+  var doneList = $('#done-column ul.card-list');
+  var card = $(this);
+
+  // Update classes
+  card.removeClass('todo');
+  card.addClass('done');
+
+  // Update listeners
+  card.off();
+  card.on('click', removeCard);
+
+  // Move to done list
+  card.appendTo(doneList);
+};
+
+var createCard = function() {
+  var todoList = $('#todo-column ul.card-list');
+  var input = $('#new-card-text');
+  var card = $('<li>')
+  card.addClass('card todo');
+  card.text(input.val());
+  card.appendTo(todoList);
+  card.on('click', finishCard);
+  input.val('');
 };
