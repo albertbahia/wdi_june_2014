e759111acbd117f90010261a44b8470a2aa2111e
hoanewton <caohoa86hp@gmail.com>
2014-07-23 18:47:31 -0400

    fix bug in keeping color unchanged and reserve the turn

--- w07/d03/hoa_newton/connectFour/connectFour.js
+++ w07/d03/hoa_newton/connectFour/connectFour.js
@@ -2,21 +2,19 @@ $(function() {
   console.log('Loaded, bro');
   startGame();
 
-  // $('.piece').click(turnRed);
-
   (function () {
   	var count = 0;
 
 	  $('.piece').click(function () {
 	  	var piece = $(this);
-	    count += 1;
-
+	    
 	    if( !piece.hasClass('red') && !piece.hasClass('black')){
 		    if (count % 2 != 0) {	
 		      piece.addClass('red'); 
 		    }else {
 		    	piece.addClass('black');
 		    }
+		    count += 1;
 		   } 
 	  });
 	})();  // Functions closing brackets
