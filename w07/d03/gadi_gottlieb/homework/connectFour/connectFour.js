11b35b4d9487d298b1619d29caadc9a458775dbc
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-07-23 23:49:56 -0400

    Completed Assignment

--- w07/d03/gadi_gottlieb/homework/connectFour/connectFour.js
+++ w07/d03/gadi_gottlieb/homework/connectFour/connectFour.js
@@ -3,13 +3,15 @@ $(function() {
   console.log('Loaded, bro');
   startGame();
   turn = 0;
-  startGame();
 });
 
 function startGame() {
   $('.piece').on('click', addPiece);
   // $('.piece').on('click', playerChange);
 }
+///////////////////////////////////
+//        Make It Stick!         //
+///////////////////////////////////
 
 var addPiece = function(){
   console.log('piece was added')
@@ -24,24 +26,28 @@ var addPiece = function(){
     }
   }
 }
-  // var redPiece = $(this).addClass('red')
-  // var blackPiece = $(this).addClass('black')
 
+
+////////////////////////////////////////////////////////////////////////
+// Create the rows for putting in pieces only on bottom.(doesn't work)//
+////////////////////////////////////////////////////////////////////////
+//   piece1 = $('.piece').eq(5),
+//   piece2 = $('.piece').eq(11),
+//   piece3 = $('.piece').eq(17),
+//   piece4 = $('.piece').eq(23),
+//   piece5 = $('.piece').eq(29),
+//   piece6 = $('.piece').eq(35),
+//   piece7 = $('.piece').eq(41)
+//   var row = $('<div="row">').appendTo('#gameboard-container')
+//   row.append(piece1, piece2, piece3, piece4, piece5, piece6, piece7)
+
+
+/////////////////////////////////////////
+// Toggles pieces between red and black//
+/////////////////////////////////////////
 //   if ($(this).hasClass('red')){
 //     $(this).removeClass('red').addClass('black');
 //   } else {
 //     $(this).removeClass('black').addClass('red');
-//   }
-// }
-
-
-
-// var playerChange = function(){
-//   if ($(this).hasClass('red')){
-//     $(this).removeClass('red').addClass('black');
-//     $(this);
-//   } else {
-//     $(this).removeClass('black').addClass('red');
-//     $(this);
 //   }
 // }
