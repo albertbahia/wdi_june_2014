3c722ee7ea05221de6943566133359cfd8059df9
jonathanporras <jonathanrporras@gmail.com>
2014-07-23 21:08:45 -0400

    Completed Assignment with Bonus

--- w07/d03/jon_porras/hw/connnectFour/connectFour.js
+++ w07/d03/jon_porras/hw/connnectFour/connectFour.js
@@ -3,6 +3,7 @@ $(function() {
   startGame();
 
   $('.piece').click(pieceClick);
+  $('#restart').click(restartGame);
 });
 
 function startGame() {
@@ -18,16 +19,60 @@ function startGame() {
 var num = 0;
 var pieceClick = function() {
   piece = $(this);
-
-if(piece.hasClass('red') || piece.hasClass('black')){
-      ;
-    } else {
-      if(num === 0){
-        piece.addClass('red');
-        num = 1;
+  if(piece.hasClass('red') || piece.hasClass('black')){
+        ;
       } else {
-        piece.addClass('black');
-        num = 0;
+        if(num === 0){
+
+          //array of id
+          var pieceIds = [];
+          var currentColumn = piece.parent().attr('id');
+            $("#" + currentColumn + ' .piece').each(function(){
+
+              //piece not already filled
+              if($(this).hasClass('red') || $(this).hasClass('black')){
+                ;
+              }else{
+
+            pieceIds.push(parseInt(this.id));
+          }
+        });
+          console.log(pieceIds);
+          //find max id
+          var highestId = Math.max.apply(null, pieceIds);
+
+          //assign class red to piece with that id
+          $('#' + highestId).addClass('red');
+
+          num = 1;
+        } else {
+
+          //array of id
+          var pieceIds = [];
+          var currentColumn = piece.parent().attr('id');
+            $("#" + currentColumn + ' .piece').each(function(){
+
+              //piece not already filled
+              if($(this).hasClass('black') || $(this).hasClass('red')){
+                ;
+              }else{
+
+            pieceIds.push(parseInt(this.id));
+          }
+        });
+          console.log(pieceIds);
+          //find max id
+          var highestId = Math.max.apply(null, pieceIds);
+
+          //assign class black to piece with that id
+          $('#' + highestId).addClass('black');
+
+          num = 0;
+        }
       }
-    }
+}
+
+var restartGame = function() {
+  $('.column .piece').attr('class', 'piece');
+  location.reload();
 }
