19ba93b2a64c31060b55c9d75ca95b4c8d8cd10d
inmakn <themousepolice@gmail.com>
2014-07-23 20:04:56 -0400

    added toggle between black and red players

--- w07/d03/kathryn_inman/connectFour/connectFour.js
+++ w07/d03/kathryn_inman/connectFour/connectFour.js
@@ -8,19 +8,48 @@ $(function() {
 
 function startGame() {
   var gamePieces = $('.piece');
+  colorToggle = 'red'
 
-  $('.piece').on('click', turnRed);
+  $('.piece').on('click', markPiece);
 
 };
 
-var turnRed = function(){
+var markPiece = function(){
   	var piece = $(this);
-  	piece.addClass('red');
-}
 
+  	piece.toggleColor = function() {
+  		if (colorToggle === 'red') {
+      		colorToggle = 'black';
+    	} else if (colorToggle === 'black') {
+      		colorToggle = 'red';
+    	} else {
+    		alert('hleP')
+    	}
+  	};
 
+	if (piece.hasClass('red')) {
+		console.log("already red");
+	} else if (piece.hasClass('black')) {
+		console.log("already black");  
+	} else {
+		if (colorToggle == 'red') {
+			piece.addClass('red');
+			piece.toggleColor();
+			console.log(colorToggle);
+		} else if (colorToggle == 'black') {
+			piece.addClass('black');
+			piece.toggleColor();
+			console.log(colorToggle);
+		} else {
+			console.log(colorToggle);
+			console.log(piece.attr('class'));
+		}
+	}
+};
 
-
+// - If it is set to red and clicked again it will not change color 
+// - If it is set to black and clicked again it will not change color
+// - An invalid click (on a colored piece) should not affect the order of the color of the next valid click. (i.e. if I am supposed to play red, and i click on a red, then next valid click should turn that gamepiece red, not black)
 
 
 
