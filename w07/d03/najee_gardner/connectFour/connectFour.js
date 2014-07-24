0d9d9782423b82ace94425d844eb9b92890444e3
truotaku <najee.s.gardner@gmail.com>
2014-07-23 23:41:46 -0400

    completed homework

--- w07/d03/najee_gardner/connectFour/connectFour.js
+++ w07/d03/najee_gardner/connectFour/connectFour.js
@@ -6,15 +6,28 @@ $(function() {
 var currentPlayer = 'red';
 
 function startGame() {
-  $('.piece').click(changePieceColor);
+  $('.piece').click(onlyChangeBottoms);
 }
 
-function changePieceColor() {
-  if(!$(this).hasClass('black') && !$(this).hasClass('red')) {
-    $(this).addClass(currentPlayer);
-    toggleTurn();
+
+// changes the piece color of only the bottom most element in the
+// connect four column
+function onlyChangeBottoms() {
+  var pieces = $(this).parent().children();
+
+  // checking if piece has no color class from the end of my array
+  // so that I may check for the bottom most elements first
+  for (var i = pieces.length - 1; i > -1; i--) {
+    if (!pieces.eq(i).hasClass('black') && !pieces.eq(i).hasClass('red')) {
+      if ($(this).get(0) === pieces.get(i)) {
+        $(this).addClass(currentPlayer);
+        toggleTurn();
+      }
+      break;
+    }
   }
 }
+
 
 function toggleTurn() {
   if (currentPlayer === 'red') {
