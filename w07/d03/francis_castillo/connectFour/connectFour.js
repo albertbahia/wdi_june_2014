683b4f5f341f7e5d0e216e23e6003fa76d74283b
fgc29 <fgc0077@gmail.com>
2014-07-24 00:59:28 -0400

    completed bonus

--- w07/d03/francis_castillo/connectFour/connectFour.js
+++ w07/d03/francis_castillo/connectFour/connectFour.js
@@ -1,10 +1,12 @@
 var colorArr = ["red"];
 var num = 1;
+
 $(function() {
   console.log('Loaded, bro');
   startGame();
   addClass();
-  $('.pieces').click(toggleColor);
+  $('.pieces').click(playPiece);
+  $('.button').click(addClass);
 
 });
 
@@ -15,15 +17,19 @@ function startGame() {
 function addClass (arguments) {
   var piecesArr = $('#gameboard-container div');
   var columns = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven']
-
+  colorArr = ["red"];
   piecesArr.each(
     function(index) {
       var idx = index + 1;
-      $(this).addClass((idx).toString());
+      $(this).addClass((idx).toString())
+      .removeClass("red")
+      .removeClass("black")
+      .removeClass("played")
+      .removeClass("playable");
 
       if (num % 7 === 0 && idx < 36) {
         $(this).attr("id", columns[num]);
-        $(this).addClass("not-playable");
+        $(this).addClass("not-playable")
         num = 1;
       } else if (idx > 35) {
         $(this).attr("id", columns[num]);
@@ -40,33 +46,28 @@ function addClass (arguments) {
 
 var changeColorTo = function(piece,color) {
   piece.addClass(color);
+  piece.removeClass('playable').addClass('played');
 }
 
-
-var toggleColor = function() {
+var playPiece = function() {
   var piece = $(this);
   var red = "red";
   var black = "black";
   var id = '#' + piece.attr('id') + '.not-playable';
   var index = (colorArr.length - 1);
+  var playablePiece = '#' + piece.attr('id') + '.playable';
 
   if (piece.hasClass("red") || piece.hasClass("black")) {
     console.log("choose another piece");
   } else {
-    if (colorArr[index] === "red"  && piece.hasClass("playable")) {
-      changeColorTo(piece, colorArr[index]);
+    if (colorArr[index] === "red" ) {
+      changeColorTo($(playablePiece), colorArr[index]);
       $(id).last().removeClass("not-playable").addClass('playable');
-
       colorArr.push(black);
-    } else if ( colorArr[index] === "black"  && piece.hasClass("playable") ) {
-      changeColorTo(piece, colorArr[index]);
+    } else if ( colorArr[index] === "black" ) {
+      changeColorTo($(playablePiece), colorArr[index]);
       $(id).last().removeClass("not-playable").addClass('playable');
       colorArr.push(red);
     }
   }
-
 };
-
-// var bottomMost = function() {
-//   $(this).
-// }
