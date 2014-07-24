8c7247b81f6f98f17241a02d54c8b255212d7fca
truotaku <najee.s.gardner@gmail.com>
2014-07-23 22:31:57 -0400

    alternating between black and white

--- w07/d03/najee_gardner/connectFour/connectFour.js
+++ w07/d03/najee_gardner/connectFour/connectFour.js
@@ -4,9 +4,14 @@ $(function() {
 });
 
 function startGame() {
+  $('.piece').click(makePieceBlack);
   $('.piece').click(makePieceRed);
 }
 
 function makePieceRed() {
   $(this).toggleClass('red');
+}
+
+function makePieceBlack() {
+  $(this).toggleClass('black', $(this).hasClass('red'));
 }
