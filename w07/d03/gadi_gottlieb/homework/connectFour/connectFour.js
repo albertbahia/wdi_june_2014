c90ea5d29b1096adcb360bc212cf1f71493ea40c
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-07-23 23:07:33 -0400

    make it stick

--- w07/d03/gadi_gottlieb/homework/connectFour/connectFour.js
+++ w07/d03/gadi_gottlieb/homework/connectFour/connectFour.js
@@ -1,46 +1,47 @@
+var turn;
 $(function() {
   console.log('Loaded, bro');
   startGame();
-
+  turn = 0;
+  startGame();
 });
 
 function startGame() {
   $('.piece').on('click', addPiece);
+  // $('.piece').on('click', playerChange);
 }
 
 var addPiece = function(){
   console.log('piece was added')
+  var piece = $(this);
+  if (piece.hasClass('red') === false && piece.hasClass('black') === false) {
+    if (turn % 2 == 0) {
+      piece.addClass('red');
+      turn++;
+    } else {
+      piece.addClass('black');
+      turn++;
+    }
+  }
+}
   // var redPiece = $(this).addClass('red')
   // var blackPiece = $(this).addClass('black')
 
-  if ($(this).hasClass('red')){
-    $(this).removeClass('red').addClass('black');
-  } else {
-    $(this).removeClass('black').addClass('red');
-  }
-}
-
-
-
-
-
-  //  hasClass('red') !== true) {
-  //   $(this).addClass('red');
-  // }
-
-
+//   if ($(this).hasClass('red')){
+//     $(this).removeClass('red').addClass('black');
+//   } else {
+//     $(this).removeClass('black').addClass('red');
+//   }
 // }
-//   if (piece !== 'rgba(250,255,255,.5)') {
-//     return piece.addClass('red')
-//     if (piece.contents() === '<div red>'){
-//       piece
-//     }
-//       {piece.addClass('black')
-//     }
-//   }
-//     piece.addClass('black');
-//   }
 
-var playerChange = function(){
 
-}
+
+// var playerChange = function(){
+//   if ($(this).hasClass('red')){
+//     $(this).removeClass('red').addClass('black');
+//     $(this);
+//   } else {
+//     $(this).removeClass('black').addClass('red');
+//     $(this);
+//   }
+// }
