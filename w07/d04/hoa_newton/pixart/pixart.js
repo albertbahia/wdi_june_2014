bfd10228972a6baa7c449dc2f4e97e5b3be5cbda
hoanewton <caohoa86hp@gmail.com>
2014-07-25 10:51:03 -0400

    finish Pixart

---
+++ w07/d04/hoa_newton/pixart/pixart.js
@@ -0,0 +1,29 @@
+$(function(){
+	$('#set-color').click(setColor);
+
+	//create 20 squares
+	for (i = 0; i < 8000; i++){
+		$('<div>').addClass('square').appendTo('body')
+	}
+
+	//change an indisidual square color to green
+	$('.square').mouseover(turnGreen)
+
+	//change color by clicking color pitch box
+	$('.color-pitch').click(changeColor)
+
+})
+
+function setColor(){
+	var color = $('#color-field').val();
+	$('.brush').css('background', color)
+}
+
+function turnGreen() {
+	$(this).toggleClass('green')
+}
+
+function changeColor(){
+	colorClass = $(this).css('background');
+	$('.brush').css('background', colorClass);
+}
