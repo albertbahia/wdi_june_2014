11ddbba7dddbde3eb55935abec7e4ec94bb67969
Khadijah Celestine <khadijah.celestine@gmail.com>
2014-07-25 18:57:11 -0700

    play piece working as expected

--- w07/d03/khadijah_celestine/connect-four/connectFour.js
+++ w07/d03/khadijah_celestine/connect-four/connectFour.js
@@ -7,51 +7,54 @@ function startGame() {
   var gamePieces = $('.piece');
   var columns = $('.column');
   var turn = true;
-  gameBoard = [ /* 0    1     2     3     4      5
-   /* 0 row */  ['a', 'c', null, null, null, null],
-   /* 1 row */  ['b', null, null, null, null, null],
-   /* 2 row */  [null, null, null, null, null, null],
-   /* 3 row */  [null, null, null, null, null, null],
-   /* 4 row */  [null, null, null, null, null, null],
-   /* 5 row */  [null, null, null, null, null, null],
-   /* 6 row */  [null, null, null, null, null, null],
-   /* 7 row */  [   7,    7,    7,    7,    7,    7]
+                /*col*/
+  var gameBoard = [ /* 0    1     2     3     4      5    6
+   /* 0 row */  ['a', 'c', null, null, null, null, null],
+   /* 1 row */  ['b', null, null, null, null, null, null],
+   /* 2 row */  ['d', null, null, null, null, null, null],
+   /* 3 row */  ['e', null, null, null, null, null, null],
+   /* 4 row */  ['f', null, null, null, null, null, null],
+   /* 5 row */  ['g', null, null, null, null, null, null],
+   /* 6 row */  [   5,    5,    5,    5,    5,    5,   5]
     ];
   
   columns.on('click', function() { 
-  col = $(this);
-  colId = col.attr('id');
-  // console.log(colId);
-  if(turn) {
-    console.log('red');
-    playPiece(gameBoard, colId, 'red');
-  } else { 
-    console.log('black');
-    playPiece(gameBoard, colId, 'black');
-  }
-  turn = !turn;
-  });
+    col = $(this);
+    colId = col.attr('id');
+    if (gameBoard[6][colId] < 0) {
+      console.log('no more plays');
+    } else {
+      if(turn) {
+        console.log('red');
+        playPiece(gameBoard, colId, 'red');
 
-  gamePieces.on('click', function() { 
+      } else { 
+        console.log('black');
+        playPiece(gameBoard, colId, 'black');
+      }
+      turn = !turn;
+    }
   });
 }
 
-//                  board,   4   , red
-var play = function(board, column, color) {
-  index = board[column][8];
-  board[column][index] = color;
-  board[column][8]--;
-};
 
+//                        board,   4   , red
 var playPiece = function(board, column, color) {
-  console.log('colid' + column, color);
-  index = board[column][6];
-  console.log(index)
-  board[column][index] = color;
-  board[column][6]--;
-  console.log('PP');
-  console.log('col: ' + column + 'row: ' + index);
-  console.log('PP');
+  if (board[6][column] < 0)
+  {
+    console.log('no more plays. sorry.');
+    return false;
+  } else {
+    var index = board[6][column];
+    console.log('index: ' + index);
+    console.log('should be null' + board[index][column]);
+    board[index][column] = color;
+    console.log('should be ' + color + ':' + board[index][column]);
+    console.log('whats in the counter column ' + board[6][column]);
+    board[6][column]--;
+    console.log('whats in the counter column now: ' + board[6][column]);
+    return true;
+  }
 };
 
 var toggle = function() {
