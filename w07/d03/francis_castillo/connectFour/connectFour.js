cb6df001346602d280918586fa7b1fb634e28ef0
fgc29 <fgc0077@gmail.com>
2014-07-27 21:17:12 -0400

    added everything except diagonal win

--- w07/d03/francis_castillo/connectFour/connectFour.js
+++ w07/d03/francis_castillo/connectFour/connectFour.js
@@ -1,22 +1,27 @@
 var colorArr = ["red"];
+
 var num = 1;
 
 $(function() {
   console.log('Loaded, bro');
+
   startGame();
+  // event listeners
   $('.pieces').click(playPiece);
   $('.button').click(startGame);
 
 });
 
-function startGame (arguments) {
+function startGame () {
   var piecesArr = $('#gameboard-container div');
-  var columns = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven']
+  var columns = [ '', 'colOne', 'colTwo', 'colThree', 'colFour', 'colFive', 'colSix', 'colSeven'];
+  var rows = [ '', 'rowOne', 'rowTwo', 'rowThree', 'rowFour', 'rowFive', 'rowSix', 'rowSeven'];
   colorArr = ["red"];
-
+  // enumerating to create indexed board and add playable/not-playable pieces
   piecesArr.each(
     function(index) {
       var idx = index + 1;
+      // resets board to start state
       $(this).addClass((idx)
         .toString())
         .removeClass("red")
@@ -37,6 +42,20 @@ function startGame (arguments) {
         $(this).addClass("not-playable");
         num+=1;
       }
+
+      if (idx >= 1 && idx >= 1 && idx < 8) {
+        $(this).addClass(rows[1]);
+      } else if (idx >= 8 && idx < 15) {
+        $(this).addClass(rows[2]);
+      } else if (idx >= 15 && idx < 22){
+        $(this).addClass(rows[3]);
+      } else if (idx >= 22 && idx < 29){
+        $(this).addClass(rows[4]);
+      } else if (idx >= 29 && idx < 36){
+        $(this).addClass(rows[5]);
+      } else if (idx >= 36 && idx < 43){
+        $(this).addClass(rows[6]);
+      }
     }
   );
 }
@@ -51,10 +70,11 @@ var playPiece = function() {
   var piece = $(this);
   var red = "red";
   var black = "black";
-  var lastSibling = $('#' + piece.attr('id') + '.not-playable').last();
   var colorIndex = (colorArr.length - 1);
+  var lastSibling = $('#' + piece.attr('id') +
+    '.not-playable').last();
   var playablePiece = $('#' + piece.attr('id') + '.playable');
-
+  // changes colors and class of the 2 bottom most pieces!
   if (piece.hasClass("red") || piece.hasClass("black")) {
   } else {
     if (colorArr[colorIndex] === "red" ) {
@@ -69,4 +89,76 @@ var playPiece = function() {
       colorArr.push(red);
     }
   }
+  checkWinner(piece, playablePiece);
 };
+
+var checkWinner = function(piece, playablePiece) {
+
+  var colId = $('#'+ piece.attr('id') + "." +
+    colorArr[colorArr.length - 2]);
+  var col = colId.filter(
+    function(v){ return typeof v ==='number'; }).length;
+
+  var rowId = $('.' + playablePiece.attr('class').split(" ")[2] +
+    '.played' + "." + colorArr[colorArr.length - 2]);
+  var row = rowId.filter(
+    function(v){ return typeof v ==='number'; }).length;
+  console.log(rowId);
+  console.log(colId);
+
+  if (col > 3) {
+    checkWinnerColumn(colId);
+  } else if (row > 3) {
+    checkWinnerRow(rowId);
+  } else if (row > 3) {
+    checkWinnerRow(rowId);
+  }
+
+};
+
+var makeWinnersArr = function(piecesArr) {
+  var winsArr = [];
+  $.each(piecesArr, function(index) {
+      winsArr.push(parseInt(piecesArr.eq(index)
+        .attr('class')
+        .split(" ")[1], 10));
+  });
+  return winsArr.reverse();
+};
+
+var checkWinnerColumn = function(piecesArr) {
+  var winsArr = makeWinnersArr(piecesArr);
+
+  if ( (winsArr[0] - winsArr[1]) === 7 && (winsArr[2] - winsArr[3])
+    === 7 && (winsArr[1] - winsArr[2]) === 7) {
+    console.log(colorArr[colorArr.length - 2] + " wins!");
+    $('.pieces').click(playPiece).off()
+  }
+
+};
+
+var checkWinnerRow = function(piecesArr) {
+  var winsArr = makeWinnersArr(piecesArr);
+
+  if ( (winsArr[0] - winsArr[1]) === 1 && (winsArr[2] - winsArr[3])
+     === 1 && (winsArr[1] - winsArr[2]) === 1) {
+    console.log(colorArr[colorArr.length - 2] + " wins!");
+    $('.pieces').click(playPiece).off()
+
+  }
+
+};
+
+var checkWinnerDiagonally = function() {
+
+
+};
+
+var connectFourBoard = [
+  [  1,  2,  3,  4,  5,  6,  7 ],
+  [  8,  9, 10, 11, 12, 13, 14 ],
+  [ 15, 16, 17, 18, 19, 20, 21 ],
+  [ 22, 23, 24, 25, 26, 27, 28 ],
+  [ 29, 30, 31, 32, 33, 34, 35 ],
+  [ 36, 37, 38, 39, 40, 41, 42 ]
+];
