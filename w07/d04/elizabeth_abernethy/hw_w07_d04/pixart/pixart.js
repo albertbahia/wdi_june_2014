89ffa29334650745e1e6fcc8d4f35c0b1f664db6
MeetElizabeth <ecaberne@gmail.com>
2014-07-28 08:44:23 -0400

    finished pixart assignment, bonus not working yet

--- w07/d04/elizabeth_abernethy/hw_w07_d04/pixart/pixart.js
+++ w07/d04/elizabeth_abernethy/hw_w07_d04/pixart/pixart.js
@@ -3,13 +3,14 @@ $(function() {
 
   createDivs();
   $('#set-color').on('click', setColor);
-  $('.square').on('click', toGreen);
+  // $('.square').on('click', toGreen); - changes to Green
+  $('.square').on('mouseover', paint);
 });
 
 
 // Make the Pixart canvas
 var createDivs = function() {
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 8000; i++) {
     $('<div>').addClass('square').appendTo('body');
   };
 };
@@ -20,7 +21,20 @@ function setColor() {
   $('.brush').css('background', color);
 }
 
-var toGreen = function() {
-  var color = $('#color-field').val();
-  $(this).css('background-color', 'green');
+var paint = function() {
+  var color = $('.brush').css('background-color');
+  $(this).css('background-color', color);
 };
+
+// Step 4 in exercise: change all divs to green on click
+// var toGreen = function() {
+//   var color = $('#color-field').val();
+//   $(this).css('background-color', 'green');
+// };
+
+// function swatchColor() {
+//   var color = $(this).css('background');
+//   $('.brush').css('background-color', color);
+// }
+// 
+// function
