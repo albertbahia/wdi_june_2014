01d77e7ced3d21df3f289bedc0e6b1b0099987ee
aaackerman <aaackerman@yahoo.com>
2014-07-28 00:45:53 -0400

    Started using Hari's solution instead. Still not completely working win checking.

--- w07/d04/alexandra_ackerman/hw_w07_d04/connect_four/connectFour.js
+++ w07/d04/alexandra_ackerman/hw_w07_d04/connect_four/connectFour.js
@@ -1,66 +1,110 @@
 $(function() {
-  console.log('Loaded, bro');
+  console.log('Loaded bro.');
   startGame();
-  $('#button').on('click', clearGameboard);
 });
 
-function startGame() {
-  var gamePieces = $('.piece');
-};
-
-
-function startGame() {
-  var gamePieces = $('.piece');
+var startGame = function() {
   var turn = 0;
-  gamePieces.click(function() {
-    var piece = findPiece(parseInt(this.id));
-    if(piece) {
-      var nextPiece = piece.attr('id') - 7;
+
+  // A semi transparent div that is placed right on
+  // top of a circle through css
+  var highlight = $('<div>').addClass('highlight');
+
+  $('.circle').click(function() {
+    var piece = $(this);
+    var pieceToPlay = lowestPiece(piece.parent());
+    if (isPlayable(piece) && pieceToPlay) {
+      // Append the red or black class
+      // depending on whether turn is odd or even
       if (turn % 2 === 0) {
-        piece.addClass('red').toggleClass('playable');
-        turn += 1;
-        setNext(nextPiece);
-      } else if (turn % 2 === 1 ) {
-        piece.addClass('black').toggleClass('playable');
-        turn += 1;
-        setNext(nextPiece);
+        pieceToPlay.addClass('red');
+      } else {
+        pieceToPlay.addClass('black');
+      }
+
+      turn++;
+
+      // UI - move the highlight up
+      if (isPlayable(piece)) {
+        highlight.appendTo(pieceToPlay.prev());
       }
     }
-  })
-  console.log(gamePieces);
-};
+  });
 
-function setNext(index) {
-  if (index >= 0 ) {
-    $('#' + index).addClass('playable')
-  }
-};
-
-function findPiece(clickedID) {
-  for (var i = 0; i < 7; i++) {
-    console.log(clickedID);
-    if($('#' + clickedID).hasClass('playable')) {
-      return $('#' + clickedID);
+  // ---------------------
+  // This is just cool UI stuff.
+  // Not necessary for solution.
+  $('.circle').mouseenter(function() {
+    var lastPiece = lowestPiece($(this).parent());
+    if (isPlayable($(this))) {
+      highlight.appendTo(lastPiece);
     } else {
-      clickedID += 7;
+      highlight.remove();
+    }
+  });
+
+  $('.circle').mouseleave(function() {
+    highlight.remove();
+  });
+  // ---------------------
+}
+
+// Accepts a piece
+// Returns true if piece is vacant
+// Returns false if piece is filled
+var isPlayable = function(piece) {
+  return !(piece.hasClass('red') || piece.hasClass('black'));
+}
+
+// Accepts a column
+// Returns the lowest playable piece in the same column
+var lowestPiece = function(column) {
+  var columnPieces = column.children();
+  for (var i = columnPieces.length - 1; i >= 0; i--) {
+    if (isPlayable(columnPieces.eq(i))) {
+      return columnPieces.eq(i);
     }
   }
-  return null;
+  return false;
+}
+
+var checkWin = function(circle, )
+
+var sameTogether = function(circle1, circle2) {
+  var red = circle1.hasClass('red') && circle2.hassClass('red');
+  var black = circle1.hasClass('red') && circle2.hassClass('red');
+  return (red || black);
 };
 
-// function clearGameboard() {
-//   var pieces = $('.piece');
-//   for (var i = 0; i < pieces.length; i++) {
-//     pieces.removeClass('red').removeClass('black');
-//   }
-// };
-
-var redTogether = function (piece1, piece2) {
-  var reds = piece1.hasClass('red') && piece2.hasClass('red');
-  return reds;
+var circleBelow = function(circle) {
+  var inColumn = circle.siblings();
+  var row = inColumn.index(circle);
+  //gets row, gets column, adds 1 to index of 
+  //circle in column to get one below
+  return inColumn.eq(row + 1);
 };
 
-var blackTogether = function(piece1, piece2) {
-  var black = piece1.hasClass('black') && piece2.hasClass('black');
+var circleLeft = function(circle) {
+  var column = circle.parent();
+  var row = column.children.index(circle);
+  return column.prev().children().eq(row);
 };
 
+var circleRight = function(circle) {
+  var column = circle.parent();
+  var row = column.children.index(circle);
+  return column.next().children().eq(row);
+};
+
+var bottomLeft = function(circle) {
+  return circleBelow(circleLeft(circle));
+};
+
+var bottomRight = function(circle) {
+  return circleBelow(circleRight(circle));
+};
+
+var topLeft = function(circle) {
+  var column = circle.parent();
+  var row = column.children.index(circle);
+};
