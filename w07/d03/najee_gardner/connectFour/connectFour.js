116453d050e8555e63b03da95064bf55b16934b5
truotaku <najee.s.gardner@gmail.com>
2014-07-28 08:49:53 -0400

    added comments for convoluted code

--- w07/d03/najee_gardner/connectFour/connectFour.js
+++ w07/d03/najee_gardner/connectFour/connectFour.js
@@ -1,37 +1,37 @@
 $(function() {
   console.log('Loaded, bro');
-  startGame();
 
+  startGame();
 });
 
 
 function startGame() {
-
   $('.piece').click(makePlay);
 }
+
 
 var currentPlayer = 'red';
 var gameOver = false;
 
-// changes the piece color of only the bottom most element in the
-// connect four column
+
 function makePlay() {
+
+  // stops the game if gameOver true
   if (gameOver) {
     return undefined;
   }
 
-  var currentPiece = $(this);
-  var pieces = currentPiece.parent().children();
+  var pieces = $(this).parent().children();
 
-  // checking if piece has no color class from the end of my array
-  // so that I may check for the bottom most elements first
+  // moves backward through array of pieces that are in a column and changes
+  // the color of the first piece that doesn't have a color class
   for (var i = pieces.length - 1; i > -1; i--) {
     if (!pieces.eq(i).hasClass('black') && !pieces.eq(i).hasClass('red')) {
 
       pieces.eq(i).addClass(currentPlayer);
       toggleTurn();
 
-      checkAndDisplayWinner(currentPiece);
+      checkAndDisplayWinner(pieces.eq(i));
       break;
     }
   }
@@ -47,9 +47,14 @@ function toggleTurn() {
 }
 
 
+// checkCol, checkRow, and the two checkDiag functions each go through three steps.
+// Step 1: the pieces for the row, column or left/right diagonal are grouped together in order.
+// Step 2: the array of pieces (jQuery objects) is converted to a simple array of 'r', 'b', and 'n' values
+// Step 3: the array is checked for 4 of the same consecutive letters.
+
+
 function checkCol(piece) {
   var colPieces = piece.parent().children();
-
   var newColumn = convertPiecesToSimpleArray(colPieces);
   var winner = checkSimpleArrayWinner(newColumn);
 
@@ -69,15 +74,20 @@ function checkRow(piece) {
 
 
 function checkDiagLeftRight(piece) {
+  // step 1 in the three step process
   var columnIndex = parseInt(piece.parent().attr('class').split(' ')[1]);
   var rowIndex = parseInt(piece.attr('class').split(' ')[1]);
 
+  // get the theoretical starting row of the diagonal at the first column
+  // (also adjusts for the fact that rows start at 0 in array form)
   var rowStart = rowIndex - columnIndex;
 
   var columns = $('.column');
+
+  // empty jquery element that will be filled with pieces
   var pieces = $();
 
-  // creates array of piece elements from the left to right cross
+  // loop through columns and and rows at the same time to grab diagonals
   for (var i = 0; i < columns.length; i++) {
     var diagPiece = columns.eq(i).children().eq(rowStart);
     if (rowStart >= 0 && diagPiece != undefined) {
@@ -85,6 +95,7 @@ function checkDiagLeftRight(piece) {
     }
     rowStart++
   }
+  // end of step 1 -------------
 
   var newDiag = convertPiecesToSimpleArray(pieces);
   var winner = checkSimpleArrayWinner(newDiag);
@@ -94,16 +105,18 @@ function checkDiagLeftRight(piece) {
 }
 
 function checkDiagRightLeft(piece) {
+  // step 1 of the 3 step process
   var columnIndex = parseInt(piece.parent().attr('class').split(' ')[1]);
   var rowIndex = parseInt(piece.attr('class').split(' ')[1]);
 
+  // get the theoretical ending row of the diagonal at the last column.
+  // (adjusted for array starting at 0)
   var rowEnd = (rowIndex - (7 - columnIndex)) - 1;
 
   var columns = $('.column');
   var pieces = $();
 
-
-  // creates array of piece elements from the right to left cross
+  // loop through columns and arrays simultaneously
   for (var i = columns.length - 1; i >= 0; i--) {
     var diagPiece = columns.eq(i).children().eq(rowEnd);
     if (rowEnd >= 0 && diagPiece != undefined) {
@@ -112,6 +125,7 @@ function checkDiagRightLeft(piece) {
 
     rowEnd++;
   }
+  // end of step 1 -------------------
 
   var newDiag = convertPiecesToSimpleArray(pieces);
   var winner = checkSimpleArrayWinner(newDiag);
@@ -156,6 +170,7 @@ function checkSimpleArrayWinner(array) {
 }
 
 function checkAndDisplayWinner(piece) {
+  // check each line for winner
   var colWinner = checkCol(piece);
   var rowWinner = checkRow(piece);
   var diagLeftRightWinner = checkDiagLeftRight(piece);
@@ -172,6 +187,7 @@ function checkAndDisplayWinner(piece) {
     winner = diagRightLeftWinner;
   }
 
+  // create heading to display winner based on winner
   if (winner === 'r' && !$('h2').hasClass('winner'))   {
     $('<h2 class="winner">').text('RED Wins...Fatality').appendTo('#gameboard-container');
     gameOver = true;
