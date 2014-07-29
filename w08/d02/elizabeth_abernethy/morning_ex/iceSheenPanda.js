26664e344b749d4cbbefa36ad7ada3d36f99b8af
MeetElizabeth <ecaberne@gmail.com>
2014-07-29 10:04:13 -0400

    finished morning exercise : ice sheen panda

--- w08/d02/elizabeth_abernethy/morning_ex/iceSheenPanda.js
+++ w08/d02/elizabeth_abernethy/morning_ex/iceSheenPanda.js
@@ -1,25 +1,32 @@
 $(function() {
   console.log('Loaded, bro');
   var body = $("body");
-  $('<img class="ice" src="http://nicenicejpg.com/200/200">');
-  $('<img class="sheen" src="img src="http://placesheen.com/200/200">');
-  $('<img class="panda" src="http://media.npr.org/assets/img/2012/01/04/ap99121501386_custom-feedbb6efa738efee47e7828e805758dc427fa60-s6-c30.jpg">');
+
+  var ice = $('<img>', {class: "ice", src: "http://nicenicejpg.com/200/200"});
+  var sheen = $('<img>', {class: "sheen", src: "http://placesheen.com/200/200"});
+  var panda = $('<img>', {class: "panda", src: "http://media.npr.org/assets/img/2012/01/04/ap99121501386_custom-feedbb6efa738efee47e7828e805758dc427fa60-s6-c30.jpg"});
+  body.append(ice).append(sheen).append(panda);
+
+  var title = $('<h1>');
+  body.append(title);
+
+  // within the mousemove
+  $(window).mousemove(function(event) {
+    var xCoord = event.pageX;
+    var yCoord = event.pageY;
+    title.text("x:" + xCoord + " y:" + yCoord);
+    ice.css({"margin-left": xCoord * .5, "margin-top": yCoord * .25 });
+    sheen.css({"margin-left": xCoord * .6, "margin-top": yCoord * .4 });
+    panda.css({"margin-left": xCoord * .4, "margin-top": yCoord * .5 });
+    body.css('background', changeColor(xCoord, yCoord));
+  });
+
 });
 
-
-// function createImages() {
-//   var ice = $('img class="ice">').appendTo('body')
-//     .append(<'img src="http://nicenicejpg.com/200/200">');
-// 
-//   var sheen = $('img class="sheen">').appendTo('body')
-//     .append(<'img src="http://placesheen.com/200/200">');
-//
-//   var panda = $('img class="panda">').appendTo('body')
-//     .append(<'img src="http://media.npr.org/assets/img/2012/01/04/ap99121501386_custom-feedbb6efa738efee47e7828e805758dc427fa60-s6-c30.jpg">');
-//
-//
-// //   var ice = $('<img/>').addClass('ice').appendTo('body');
-// //   var sheen = $('<img/>').addClass('sheen').appendTo('body');
-// //   var panda = $('<img/>').addClass('panda').appendTo('body');
-//
-// }
+function changeColor(x, y){
+  var hue = x / parseInt($(window).width()) * 360;
+  var saturation = ",100%,"
+  var lightness = y / parseInt($(window).height()) * 100;
+  var hsl = "hsl("+hue+saturation+lightness+"%)";
+  return hsl;
+}
