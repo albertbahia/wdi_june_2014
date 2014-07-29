22a59facb015cdb5ebde10487a0d3c917ea9aa15
MeetElizabeth <ecaberne@gmail.com>
2014-07-29 09:21:23 -0400

    morning exercise ice sheen panda added

---
+++ w08/d02/elizabeth_abernethy/morning_ex/iceSheenPanda.js
@@ -0,0 +1,25 @@
+$(function() {
+  console.log('Loaded, bro');
+  var body = $("body");
+  $('<img class="ice" src="http://nicenicejpg.com/200/200">');
+  $('<img class="sheen" src="img src="http://placesheen.com/200/200">');
+  $('<img class="panda" src="http://media.npr.org/assets/img/2012/01/04/ap99121501386_custom-feedbb6efa738efee47e7828e805758dc427fa60-s6-c30.jpg">');
+});
+
+
+// function createImages() {
+//   var ice = $('img class="ice">').appendTo('body')
+//     .append(<'img src="http://nicenicejpg.com/200/200">');
+// 
+//   var sheen = $('img class="sheen">').appendTo('body')
+//     .append(<'img src="http://placesheen.com/200/200">');
+//
+//   var panda = $('img class="panda">').appendTo('body')
+//     .append(<'img src="http://media.npr.org/assets/img/2012/01/04/ap99121501386_custom-feedbb6efa738efee47e7828e805758dc427fa60-s6-c30.jpg">');
+//
+//
+// //   var ice = $('<img/>').addClass('ice').appendTo('body');
+// //   var sheen = $('<img/>').addClass('sheen').appendTo('body');
+// //   var panda = $('<img/>').addClass('panda').appendTo('body');
+//
+// }
