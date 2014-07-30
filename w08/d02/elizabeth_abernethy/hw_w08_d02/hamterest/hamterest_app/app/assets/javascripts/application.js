91e40e0e385d97b98e74773aab66210575d92fee
MeetElizabeth <ecaberne@gmail.com>
2014-07-29 20:01:45 -0400

    finished homework assignment: modals close and empty each time

--- w08/d02/elizabeth_abernethy/hw_w08_d02/hamterest/hamterest_app/app/assets/javascripts/application.js
+++ w08/d02/elizabeth_abernethy/hw_w08_d02/hamterest/hamterest_app/app/assets/javascripts/application.js
@@ -22,8 +22,9 @@ $(function(){
   $('#footer').on('click', showMore);
   $('#posts-container').on('click', 'span.remove', removePost);
   $('#new-post').on('click',addPost)
-  $('#posts-container').on('click', 'div.image', openModal);
+  $('#posts-container').on('click', 'img', openModal);
   $('#modal').on('click', '#close', closeModal);
+  $('#modal').on('click', '#update-button', updatePost);
 });
 
 function fetchPosts() {
@@ -56,6 +57,7 @@ function renderPost(data) {
   var content = $('<p>').addClass('content');
   var category = $('<a>').attr('href','#').addClass('category');
 
+  card.attr('data-id', data.id);
   title.text(data.title);
   author.text(data.author);
   image.attr('src', data.image_url);
@@ -158,13 +160,86 @@ function fetchOne() {
 }
 
 function openModal() {
+  // Wipe out modal each time to start over
+  closeModal();
+
   $('#modal').show();
-  var imageDiv = $(this);
-  var clickedImage = (imageDiv).find('img');
+  var post = $(this).closest('.card');
+  var postID = post.data('id');
+
+  var modal = $('#modal');
+  var clickedImage = $(this);
+  var clickedImageSrc = $(this).attr('src');
   var cardInfoDiv = $('#card-info');
-  cardInfoDiv.append(clickedImage);
+  var modalImage = $('<img class="modal-image"/>').attr('src', clickedImageSrc);
+  var cardUpdateDiv = $('#card-update');
+
+  cardInfoDiv.append(modalImage);
+  cardUpdateDiv.attr('data-id', postID);
+
+  $.get('/posts/' + postID, function(postObject) {
+    var titleInput = $('<input type="text" id="edit-title">');
+    var imageInput = $('<input type="text" id="edit-image">');
+    var contentInput = $('<input type="text" id="edit-content">');
+    var categoryInput = $('<input type="text" id="edit-category">');
+    var authorInput = $('<input type="hidden" id="edit-author">');
+
+    titleInput.val(postObject.title);
+    imageInput.val(postObject.image_url);
+    contentInput.val(postObject.content);
+    categoryInput.val(postObject.category);
+    authorInput.val(postObject.author);
+
+    cardUpdateDiv.append(titleInput).append(imageInput).append(contentInput).append(categoryInput).append(authorInput);
+  });
+
+  var updateButton = $('<button id="update-button">');
+    updateButton.text('Update this post');
+  modal.append(updateButton);
+
 }
 
+
+function updatePost() {
+  var postID = $('#card-update').data('id');
+
+  var updatedTitle = $('#edit-title').val();
+  var updatedImage = $('#edit-image').val();
+  var updatedContent = $('#edit-content').val();
+  var updatedCategory = $('#edit-category').val();
+  var updatedAuthor = $('#edit-author').val();
+
+  var params = {
+    post: {
+      title: updatedTitle,
+      image_url: updatedImage,
+      content: updatedContent,
+      category: updatedCategory,
+      author: updatedAuthor
+    }
+  };
+
+  $.ajax('/posts/' + postID, { type: "PUT", data: params })
+    .done(function(post) {
+      closeModal();
+
+      var card = $('#posts-container').find('.card[data-id="' + post.id + '"]');
+
+      card.find('.title').text(post.title);
+      card.find('.image').attr('src', post.image_url);
+      card.find('.content').text(post.content);
+      card.find('.category').text(post.category);
+    });
+
+}
+
+
 function closeModal() {
+  var image = $('#card-info').find('img');
+  var allInputs = $('#card-update').find('input');
+  var button = $('#update-button');
+  allInputs.remove();
+  image.remove();
+  button.remove();
   $('#modal').hide();
 }
