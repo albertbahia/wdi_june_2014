f5db7cac7b71fda95a0d2859e62efd89dc26e0ca
MeetElizabeth <ecaberne@gmail.com>
2014-07-30 16:35:29 -0400

    added search feature

--- w08/d03/elizabeth_abernethy/hw_w08_d03/app/controllers/products_controller.rb
+++ w08/d03/elizabeth_abernethy/hw_w08_d03/app/controllers/products_controller.rb
@@ -1,12 +1,18 @@
 class ProductsController < ApplicationController
 
   def index
+    @products = Product.all
   end
 
   def search
+    @products = Product.where("name LIKE '%#{params[:search]}%'")
   end
 
   def filter
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :quantity, :price, :category)
   end
 
 end
