74094588e299bc22665bac28be6a2929f194c690
hoanewton <caohoa86hp@gmail.com>
2014-07-31 08:54:53 -0400

    finish shoppr

--- w08/d03/hoa_newton/shoppr_app/app/controllers/products_controller.rb
+++ w08/d03/hoa_newton/shoppr_app/app/controllers/products_controller.rb
@@ -4,4 +4,14 @@ class ProductsController < ApplicationController
     @products = Product.all 
   end
 
+  def search
+    @products = Product.where("name LIKE (?)", "%#{params[:search]}%")
+    render 'index'
+  end
+
+  def filter
+    @products = Product.where({ category: params[:category]})
+    render 'index'
+  end
+
 end
