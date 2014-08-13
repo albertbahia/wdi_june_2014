656cbcea8e1817dbe2ddcb64fde1610b48aa76d5
MeetElizabeth <ecaberne@gmail.com>
2014-08-13 11:43:51 -0400

    commented out answers

--- w10/d03/elizabeth_abernethy/morning_ex/lib/doubleSort.js
+++ w10/d03/elizabeth_abernethy/morning_ex/lib/doubleSort.js
@@ -45,99 +45,99 @@ function doubleSort(arrayOfStrings) {
 
 // ------------------------- NAJEE GARDNER  -----------------------------
 
-function sort(array) {
-  var numbers = [];
-  var words = [];
-  var placement = [];
-
-  for (var i = 0; i < array.length; i++) {
-
-    if (parseInt(array[i])) {
-      numbers.push(array[i]);
-      placement.push('number');
-    } else {
-      words.push(array[i]);
-      placement.push('word')
-    }
-    count++;
-  }
-
-  numberSwap = true
-  wordSwap = true
-
-  var whileCount = 0;
-  while (numberSwap === true || wordSwap === true) {
-    numberSwap = false
-    wordSwap = false
-
-    var forCount = 0;
-
-    for (var i = 0; i < numbers.length; i++) {
-      if (parseInt(numbers[i]) > parseInt(numbers[i + 1])) {
-        var temp = numbers[i];
-        numbers[i] = numbers[i + 1];
-        numbers[i + 1] = temp;
-        numberSwap = true;
-      }
-      forCount++;
-    }
-
-    for (var i = 0; i < words.length; i++) {
-      if (words[i] > words[i + 1]) {
-        var temp = words[i];
-        words[i] = words[i + 1];
-        words[i + 1] = temp;
-        wordSwap = true;
-      }
-    }
-    whileCount++;
-  }
-
-  var sortedArray = [];
-
-  for (var i = 0; i < placement.length; i++) {
-    if (placement[i] === 'number') {
-      sortedArray.push(numbers.shift());
-    } else {
-      sortedArray.push(words.shift());
-    }
-  }
-
-  return sortedArray;
-}
+// function sort(array) {
+//   var numbers = [];
+//   var words = [];
+//   var placement = [];
+//
+//   for (var i = 0; i < array.length; i++) {
+//
+//     if (parseInt(array[i])) {
+//       numbers.push(array[i]);
+//       placement.push('number');
+//     } else {
+//       words.push(array[i]);
+//       placement.push('word')
+//     }
+//     count++;
+//   }
+//
+//   numberSwap = true
+//   wordSwap = true
+//
+//   var whileCount = 0;
+//   while (numberSwap === true || wordSwap === true) {
+//     numberSwap = false
+//     wordSwap = false
+//
+//     var forCount = 0;
+//
+//     for (var i = 0; i < numbers.length; i++) {
+//       if (parseInt(numbers[i]) > parseInt(numbers[i + 1])) {
+//         var temp = numbers[i];
+//         numbers[i] = numbers[i + 1];
+//         numbers[i + 1] = temp;
+//         numberSwap = true;
+//       }
+//       forCount++;
+//     }
+//
+//     for (var i = 0; i < words.length; i++) {
+//       if (words[i] > words[i + 1]) {
+//         var temp = words[i];
+//         words[i] = words[i + 1];
+//         words[i + 1] = temp;
+//         wordSwap = true;
+//       }
+//     }
+//     whileCount++;
+//   }
+//
+//   var sortedArray = [];
+//
+//   for (var i = 0; i < placement.length; i++) {
+//     if (placement[i] === 'number') {
+//       sortedArray.push(numbers.shift());
+//     } else {
+//       sortedArray.push(words.shift());
+//     }
+//   }
+//
+//   return sortedArray;
+// }
 
 
 
 // ------------------------- KHADIJAH  -----------------------------
 
 
-
-var doubleSort = function(arr) {
-  var nums = [];
-  var strs = [];
-  var sorted = [];
-  for( i in arr) {
-    isNaN(arr[i]) ? strs.push(arr[i]) : nums.push(arr[i]);
-  }
-  strs.sort();
-  nums.sort(function(a,b) { return a - b; });
-
-  var counter_s = 0;
-  var counter_i = 0;
-
-  for( i in arr) {
-    if(isNaN(arr[i])) {
-      sorted[i] = strs[counter_s];
-      counter_s++;
-    } else {
-      sorted[i] = nums[counter_i];
-      counter_i++;
-    }
-  }
-  console.log(sorted);
-};
-
-doubleSort(["sugar", "butter", "egg"]);
-doubleSort(["12", "10", "3", "4", "1"]);
-doubleSort(["16", "8", "4", "salt", "baking", "soda"]);
-doubleSort(["2", "4", "banana", "1", "vanilla", "flour"]);
+//
+// var doubleSort = function(arr) {
+//   var nums = [];
+//   var strs = [];
+//   var sorted = [];
+//   for( i in arr) {
+//     isNaN(arr[i]) ? strs.push(arr[i]) : nums.push(arr[i]);
+//   }
+//   strs.sort();
+//   nums.sort(function(a,b) { return a - b; });
+//
+//   var counter_s = 0;
+//   var counter_i = 0;
+//
+//   for( i in arr) {
+//     if(isNaN(arr[i])) {
+//       sorted[i] = strs[counter_s];
+//       counter_s++;
+//     } else {
+//       sorted[i] = nums[counter_i];
+//       counter_i++;
+//     }
+//   }
+//   console.log(sorted);
+// };
+//
+// doubleSort(["sugar", "butter", "egg"]);
+// doubleSort(["12", "10", "3", "4", "1"]);
+// doubleSort(["16", "8", "4", "salt", "baking", "soda"]);
+// doubleSort(["2", "4", "banana", "1", "vanilla", "flour"]);
