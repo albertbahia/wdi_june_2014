685e01b6792aa72c2ef43874857205a2c1632f61
alevsxm <alexmlevine@gmail.com>
2014-08-13 22:42:47 -0400

    stated hw

---
+++ w10/d03/alexander_levine/hangman/app/assets/javascripts/hangman.js
@@ -0,0 +1,73 @@
+var game = {
+  initialize: function(){
+    console.log('initialized');
+    $('body').on('click', '#guess', guessLetter);
+    $('body').on('click', '#giveup', giveUp);
+    this.fetchWord();
+    },
+  render: function(){
+    console.log('render');
+    $('#container').append(this.template(this.board));
+    },
+  fetchWord: function(){
+    console.log('fetching');
+    var currentGame = this;
+    $.get('/fetch_word').done(function(data) {
+      currentGame.board.word = data.word.split('');
+      currentGame.board.correct = [];
+      currentGame.board.correct.length = currentGame.board.word.length;
+      currentGame.board.guessesLeft = 8;
+      currentGame.board.guessed = [];
+      currentGame.render();
+    });
+    },
+  template: HandlebarsTemplates['hangman/board'],
+  board: {correct: null,
+          guessesLeft: null,
+          guessed: null}
+};
+
+function guessLetter() {
+  var correctAnswer = false;
+  var answer = game.board.word;
+  for (var i = 0; i < answer.length; i++) {
+    if ($('#guessed-letter').val() === answer[i]) {
+      game.board.correct.splice(i, 1, ($('#guessed-letter').val()));
+      correctAnswer = true;
+    }
+  }
+  if (correctAnswer === false) {
+    game.board.guessesLeft -= 1;
+  }
+  game.board.guessed.push($('#guessed-letter').val());
+  $('#container').empty();
+  game.render();
+  checkWin();
+  checkLost();
+}
+
+function checkWin() {
+  var match = false;
+  var correctArray = game.board.correct;
+  for (var i = 0; i < correctArray.length; i++) {
+    if (correctArray[i]) {
+      match = true;
+    } else {
+      return false;
+    }
+  }
+  if (match === true) {
+    alert("You win!!!");
+  }
+}
+
+function checkLost() {
+  if (game.board.guessesLeft === 0) {
+    alert("You lose! The word was " + game.board.word.join(''));
+  }
+}
+
+function giveUp() {
+  alert("Way to give up... the word was " + game.board.word.join(''));
+  location.reload();
+}
