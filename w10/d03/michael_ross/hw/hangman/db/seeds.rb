0e0087cdc1dc4457a63839d0045e96f2ada06b5a
MALRMALR <michaelalross@gmail.com>
2014-08-13 23:08:43 -0400

    completed assignment

---
+++ w10/d03/michael_ross/hw/hangman/db/seeds.rb
@@ -0,0 +1,3 @@
+['ruby', 'rails', 'javascript', 'array', 'hash', 'sinatra', 'model', 'controller', 'view', 'devise', 'authentication', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'development', 'data', 'design', 'inheritance', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'deployment', 'database'].each do |word|
+  Word.create(word: word)
+end
