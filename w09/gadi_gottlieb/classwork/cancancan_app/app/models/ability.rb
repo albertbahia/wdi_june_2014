ed47b12ee888843670cd41f251bcc01aa566c69c
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-08-14 00:15:33 -0400

    Classwork

---
+++ w09/gadi_gottlieb/classwork/cancancan_app/app/models/ability.rb
@@ -0,0 +1,15 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, Article
+    can :create, Article
+    can :update, Article do |article|
+      user == article.user
+    end
+
+    can :destroy, Article do |article|
+      user == article.user
+    end
+  end
+end
