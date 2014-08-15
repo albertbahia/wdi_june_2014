a673aa1d7a7bd86b134bf64d7d2d6dd64ed85f8f
jonathanporras <jonathanrporras@gmail.com>
2014-08-14 20:20:40 -0400

    updated account.js

--- w10/d04/jon_porras/hw/bank_account/account.js
+++ w10/d04/jon_porras/hw/bank_account/account.js
@@ -10,10 +10,18 @@ Account.prototype.deposit = function(amount) {
 }
 
 Account.prototype.withdraw = function(amount, bank) {
+var remainingBalance = amount - bank.checking.balance;
+
   if(amount<=this.balance) {
     this.balance -= amount;
   } else if(amount>this.balance && this.type === 'checking' && bank.savings.balance>=amount) {
     bank.savings.balance -= amount;
+  } else if(remainingBalance < bank.savings.balance) {
+    bank.checking.balance = 0;
+    bank.savings.balance -= remainingBalance;
+  } else if(remainingBalance===bank.savings.balance){
+    bank.savings.balance = 0;
+    bank.checking.balance = 0;
   } else {
     alert("you aint got the funds bruh.")
   }
