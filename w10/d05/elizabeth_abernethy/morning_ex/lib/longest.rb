929a4a220c2face024415683af9170c5592dd213
MeetElizabeth <ecaberne@gmail.com>
2014-08-15 09:54:24 -0400

    added morning exercise: method return longest word in string, using reg expressions

---
+++ w10/d05/elizabeth_abernethy/morning_ex/lib/longest.rb
@@ -0,0 +1,46 @@
+# Write a method that takes one argument (string)
+# and returns the longest word from that string
+
+jubjub = "Beware the Jubjub bird, and shun the frumios Bandersnatch!"
+jabberwock = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!!!!!!!!!"
+
+def longest_word(string)
+  split_words = string.gsub!(/[^a-z0-9\s]/i, '').split(' ')
+  longest = split_words.max_by { |x| x.length }
+end
+
+# Notes on Regular Expressions:
+#  ^ = "everything that isn't..."
+#  \s = white spaces
+#  /i = makes it not case sensitive
+#  replaces with white space
+
+# split > take a string and turn it into an array specified by what you want
+
+
+
+# ----------------- JON PORRAS -------------------------
+
+
+# def longest_word(string)
+#   holder = ""
+#   string.gsub(/[^a-z0-9\s]/i, '').split(" ").each do |word|
+#     if word.length > holder.length
+#       holder = word
+#     end
+#   end
+#   return holder
+# end
+
+
+# ---------------- PETER PAK ---------------------------
+
+# def longest_word(string)
+#   string.scan(/\w+/).max_by{ |word| word.length }
+# end
+
+# -- grabs everything that is either a letter or a number / no spaces, punctuation
+# -- underscores are included
+
+# -- scan > find stuff you want it to find and leave it in, remove everything else
+# -- when scan finds something that doesn't fit your criteria, it breaks it
