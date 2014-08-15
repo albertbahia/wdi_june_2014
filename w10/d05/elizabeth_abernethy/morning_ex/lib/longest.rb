7e17c830c86c23802822102861347abbf9a29063
MeetElizabeth <ecaberne@gmail.com>
2014-08-15 10:16:47 -0400

    added benchmark file

--- w10/d05/elizabeth_abernethy/morning_ex/lib/longest.rb
+++ w10/d05/elizabeth_abernethy/morning_ex/lib/longest.rb
@@ -1,8 +1,8 @@
 # Write a method that takes one argument (string)
 # and returns the longest word from that string
 
-jubjub = "Beware the Jubjub bird, and shun the frumios Bandersnatch!"
-jabberwock = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!!!!!!!!!"
+# jubjub = "Beware the Jubjub bird, and shun the frumios Bandersnatch!"
+# jabberwock = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!!!!!!!!!"
 
 def longest_word(string)
   split_words = string.gsub!(/[^a-z0-9\s]/i, '').split(' ')
@@ -22,22 +22,22 @@ end
 # ----------------- JON PORRAS -------------------------
 
 
-# def longest_word(string)
-#   holder = ""
-#   string.gsub(/[^a-z0-9\s]/i, '').split(" ").each do |word|
-#     if word.length > holder.length
-#       holder = word
-#     end
-#   end
-#   return holder
-# end
+def longest_word(string)
+  holder = ""
+  string.gsub(/[^a-z0-9\s]/i, '').split(" ").each do |word|
+    if word.length > holder.length
+      holder = word
+    end
+  end
+  return holder
+end
 
 
 # ---------------- PETER PAK ---------------------------
 
-# def longest_word(string)
-#   string.scan(/\w+/).max_by{ |word| word.length }
-# end
+def longest_word(string)
+  string.scan(/\w+/).max_by{ |word| word.length }
+end
 
 # -- grabs everything that is either a letter or a number / no spaces, punctuation
 # -- underscores are included
@@ -49,26 +49,26 @@ end
 
 # ---------------- NAJEE GARDNER -----------------------
 
-# def longest_word(string)
-#   word = ''
-#   counter = 0
-#   letter_count = 0
-#   longest_word = ''
-#
-#   while counter < string.length
-#     if string[counter] =~ /[a-zA-Z]/
-#       letter_count += 1
-#       word += string[counter]
-#     end
-#
-#     if string[counter] == ' ' || counter == string.length - 1
-#       longest_word = word if word.length > longest_word.length
-#       word = ''
-#       letter_count = 0
-#     end
-#
-#     counter += 1
-#   end
-#
-#   longest_word
-# end
+def longest_word(string)
+  word = ''
+  counter = 0
+  letter_count = 0
+  longest_word = ''
+
+  while counter < string.length
+    if string[counter] =~ /[a-zA-Z]/
+      letter_count += 1
+      word += string[counter]
+    end
+
+    if string[counter] == ' ' || counter == string.length - 1
+      longest_word = word if word.length > longest_word.length
+      word = ''
+      letter_count = 0
+    end
+
+    counter += 1
+  end
+
+  longest_word
+end
