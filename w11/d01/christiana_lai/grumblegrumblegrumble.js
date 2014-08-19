1761f6b211d92007c1c2cb0efd1d4e08fdda6f33
clai1018 <clai1018@gmail.com>
2014-08-19 15:52:25 -0400

    Grumble

---
+++ w11/d01/christiana_lai/grumblegrumblegrumble.js
@@ -0,0 +1,79 @@
+//Create a new grumble (call it 'grumble1') with no attributes set.
+var grumblr1 = new GrumblrModel;
+
+//Set that grumbles attributes in one line.
+Grumblr.set({
+  author: 'Ryan OReilly',
+  content: 'Bresaola salami chuck doner pastrami tail biltong pork kevin rump shoulder ground round.',
+  title: 'Sharpest Cell Shiv',
+  image: 'http://www.cosmodromemag.com/files/oz.jpg',
+  avatar: 'http://cdn.gunaxin.com/wp-content/uploads/2010/10/copyof484621um6.jpg' });
+
+//Save grumble1.
+grumblr1.save();
+
+//Delete grumble1.
+grumblr1.clear();
+
+//Create grumble2, this time setting the attributes when you create it.
+var grumblr2 = new GrumblrModel({author: 'Ryan OReilly', content: 'Bresaola salami chuck doner pastrami tail biltong pork kevin rump shoulder ground round.', title: 'Sharpest Cell Shiv', image: 'http://www.cosmodromemag.com/files/oz.jpg', avatar: 'http://cdn.gunaxin.com/wp-content/uploads/2010/10/copyof484621um6.jpg'});
+
+//Modify grumble2's title and content, and save it (2 lines).
+grumblr2.set({title: 'My Favorite Latrine Duty'});
+
+grumblr2.set({content: 'Oswald Penitentiary Gabagool'});
+
+//Create a new gumble collection, and call it allGrumbles.
+var allGrumblrs = new GrumblrsCollection();
+
+//Fill the allGrumbles collection with the data from the server.
+allGrumblrs.fetch();
+
+//Retrieve a specific grumble (of your choosing) from the collection and store it in a variable called 'myFavGrumble'.
+myFavGrumblr = allGrumblrs.fetch(id=1);
+
+//Remove myFavGrumble from the collection. Shed a solitary tear, but resolve to move on.
+allGrumblrs.remove('c3');
+
+//Add a brand new grumble to the collection in one line. Make sure to set its properties.
+allGrumblrs.create({author: 'Ryan OReilly', content: 'Bresaola salami chuck doner pastrami tail biltong pork kevin rump shoulder ground round.', title: 'Sharpest Cell Shiv', image: 'http://www.cosmodromemag.com/files/oz.jpg', avatar: 'http://cdn.gunaxin.com/wp-content/uploads/2010/10/copyof484621um6.jpg'});
+
+
+//Sync a collection
+Backbone.sync = function(method, model, options) {
+
+  function success(result) {
+    if (options.success) {
+      options.success(result);
+    }
+  }
+
+  function error(result) {
+    if (options.error) {
+      options.error(result);
+    }
+  }
+
+  options || (options = {});
+
+  switch (method) {
+    case 'create':
+      return grumblr.create(model, success, error);
+
+    case 'update':
+      return grumblr.update(model, success, error);
+
+    case 'patch':
+      return grumblr.patch(model, success, error);
+
+    case 'delete':
+      return grumblr.destroy(model, success, error);
+
+    case 'read':
+      if (model.cid) {
+        return grumblr.find(model, success, error);
+      } else {
+        return grumblr.findAll(model, success, error);
+      }
+  }
+};
