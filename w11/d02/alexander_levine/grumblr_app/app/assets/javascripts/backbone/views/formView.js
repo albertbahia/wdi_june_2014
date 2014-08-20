ce8cb7585bba3a8d6bc5aa0d85092a32ae16e16f
alevsxm <alexmlevine@gmail.com>
2014-08-19 20:22:45 -0400

    completed assignment

---
+++ w11/d02/alexander_levine/grumblr_app/app/assets/javascripts/backbone/views/formView.js
@@ -0,0 +1,28 @@
+var FormView = Backbone.View.extend({
+  el: '#grumble-form',
+  initialize: function() {
+    this.template = HandlebarsTemplates['grumbles/form']
+    this.listenTo
+    this.render();
+  },
+
+  events: {
+    'click button': 'addGrumble'
+  },
+
+  render: function() {
+    this.$el.empty();
+    this.$el.html(this.template());
+  },
+
+  addGrumble: function() {
+    var grumble = new GrumbleModel({
+      author: $('#grumble-form').find("input[name='author']").val(),
+      title: $('#grumble-form').find("input[name='title']").val(),
+      avatar: $('#grumble-form').find("input[name='avatar']").val(),
+      content: $('#grumble-form').find("input[name='content']").val(),
+    });
+    grumble.save();
+    grumbles.fetch({ reset: true });
+  }
+})
---
+++ w11/d02/alexander_levine/grumblr_app/app/assets/javascripts/backbone/views/grumbleView.js
@@ -0,0 +1,14 @@
+var GrumbleView = Backbone.View.extend({
+  className: 'grumble',
+  initialize: function() {
+    this.template = HandlebarsTemplates['grumbles/grumble'];
+    this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.render();
+  },
+
+  render: function() {
+    this.$el.empty();
+    this.$el.html(this.template(this.model.toJSON()));
+  }
+});
