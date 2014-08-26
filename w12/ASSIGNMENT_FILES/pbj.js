1289402536d9c9879413425244b724554e8d18b1
DrRobotmck <mckenneth.scott@icloud.com>
2014-08-26 09:34:09 -0400

    next

---
+++ w12/ASSIGNMENT_FILES/pbj.js
@@ -0,0 +1,9 @@
+var addJelly = addNutButter('peanut');
+console.log(addJelly('blueberry jam'));
+
+
+function addNutButter(butterType) {
+  return function(secondItem) {
+    return 'You made a ' + butterType + ' butter and ' + secondItem + ' sandwich.'
+  }
+}
